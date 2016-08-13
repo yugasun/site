3c2af7978fefca3ea142f5d63c31523a74d44f9d
davidwells <david@inboundnow.com>
2016-08-12 19:33:28 -0700

    add google analytics to SPA router

--- src/IndexComponent.js
+++ src/IndexComponent.js
@@ -5,6 +5,7 @@ import React, { Component, PropTypes } from 'react'
 import Helmet from 'react-helmet'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import GoogleAnalyticsTracker from './components/GoogleAnalyticsTracker/GoogleAnalyticsTracker'
 import segmentIO from './assets/segment'
 // Import global CSS before other components and their styles
 import './index.global.css'
@@ -13,6 +14,8 @@ import styles from './index.css'
 export default class IndexComponent extends Component {
   static propTypes = {
     children: PropTypes.oneOfType([ PropTypes.array, PropTypes.object ]),
+    /** React Router params **/
+    params: PropTypes.object,
   };
 
   static contextTypes = {
@@ -27,22 +30,24 @@ export default class IndexComponent extends Component {
 
     return (
       <div className={styles.layout}>
-        <style>{"@-ms-viewport { width: device-width; }"}</style>
-        <Helmet
-          meta={[
-            { property: 'og:site_name', content: pkg.name },
-            { name: 'twitter:site', content: `@${pkg.twitter}` },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-          ]}
-        />
+        <GoogleAnalyticsTracker params={this.props.params}>
+          <style>{"@-ms-viewport { width: device-width; }"}</style>
+          <Helmet
+            meta={[
+              { property: 'og:site_name', content: pkg.name },
+              { name: 'twitter:site', content: `@${pkg.twitter}` },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+            ]}
+          />
 
-        <Header />
-        {dynamicContent}
-        <Footer />
-        <div
-          id='footerScripts'
-          dangerouslySetInnerHTML={{__html: segmentIO}}
-        />
+          <Header />
+          {dynamicContent}
+          <Footer />
+          <div
+            id='footerScripts'
+            dangerouslySetInnerHTML={{__html: segmentIO}}
+          />
+        </GoogleAnalyticsTracker>
       </div>
     )
   }
