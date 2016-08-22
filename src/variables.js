d4fca91679e3449b7c20f884f72c499ccc680070
davidwells <david@inboundnow.com>
2016-08-22 16:49:09 -0700

    add css variables

--- src/variables.js
+++ src/variables.js
@@ -1,10 +1,37 @@
 /* Global CSS variables for use in CSS and JS */
 module.exports = {
-  /* Colors */
+  // -- Sizing
+  unit: '1rem',
+  navHeight: '76px',
+  // -- Colors
   yellow: '#ffd426',
   red: '#e55934',
   gray1: '#aaa',
   gray2: '#202020',
   black1: '#1c1c1c',
-  navHeight: '76px',
+  // -- Indexes
+  zIndexHighest: 300,
+  zIndexHigher: 200,
+  zIndexHigh: 100,
+  zIndexNormal: 1,
+  zIndexLow: -100,
+  zIndexLower: -200,
+  // -- Breakpoints
+  breakpointXxs: '480px',
+  breakpointXs: '600px',
+  breakpointSmTablet: '720px',
+  breakpointSm: '840px',
+  breakpointMd: '960px',
+  breakpointLgTablet: '1024px',
+  breakpointLg: '1280px',
+  breakpointXl: '1440px',
+  breakpointXxl: '1600px',
+  breakpointXxxl: '1920px',
+  // -- Animation
+  animationDuration: '.35s',
+  animationDelay: '.07s', // $animation-duration / 5,
+  animationCurveFastOutSlowIn: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  animationCurveLinearOutSlowIn: 'cubic-bezier(0, 0, 0.2, 1)',
+  animationCurveFastOutLinearIn: 'cubic-bezier(0.4, 0, 1, 1)',
+  animationCurveDefault: 'cubic-bezier(0.4, 0, 0.2, 1)',
 }
