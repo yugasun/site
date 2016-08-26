4d4bbcdf855093c701c7289b8ff86ae98bb1c1d7
davidwells <david@inboundnow.com>
2016-08-26 10:19:48 -0700

    remove console.logs

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -39,14 +39,14 @@ export default class Homepage extends Component {
   componentDidMount () {
     const auth = this.context.auth
     const loggedIn = auth.loggedIn()
-    console.log('componentDidMount loggedIn', loggedIn)
+    // console.log('componentDidMount loggedIn', loggedIn)
     // in mulesoft application XYZ
     window.addEventListener('serverlessLogin', this.handleOnLogin, false)
   }
   componentDidUpdate () {
     const auth = this.context.auth
     const loggedIn = auth.loggedIn()
-    console.log('componentDidUpdate loggedIn', loggedIn)
+    // console.log('componentDidUpdate loggedIn', loggedIn)
   }
   handleToggle = () => {
     this.setState({showModal: !this.state.showModal})
@@ -104,10 +104,9 @@ export default class Homepage extends Component {
     }
   }
   render () {
-    console.log('homepage', this.context.auth)
     const auth = this.context.auth
     const loggedIn = auth.loggedIn()
-    console.log('render', loggedIn)
+
     return (
       <Page {...this.props} fullWidth>
         <div className={styles.wrapper}>
