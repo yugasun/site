9de89ae2a8e1dcc71e7affb5bfdb072d1a4563ae
davidwells <david@inboundnow.com>
2016-09-05 20:43:46 -0700

    create dir if it doesn't exist

--- scripts/docs/empty-directory.js
+++ scripts/docs/empty-directory.js
@@ -1,16 +1,11 @@
 var fs = require('fs-extra')
 
 module.exports = function emptyDirectory (path, callBack) {
-
-  if (fs.existsSync(path)) {
-    fs.emptyDir(path, function (error) {
-      if (error) {
-        console.log('emptyDirectory fail! ' + path)
-        callBack(error)
-      }
-      callBack && callBack(null)
-    })
-  } else {
-    callBack && callBack('error no dir found')
-  }
+  fs.emptyDir(path, function (error) {
+    if (error) {
+      console.log('emptyDirectory fail! ' + path)
+      callBack(error)
+    }
+    callBack && callBack(null)
+  })
 }
