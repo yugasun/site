33346ac5c19a58a1c9aa4429350b3c312e51dac3
davidwells <david@inboundnow.com>
2016-09-28 16:17:22 -0700

    resolve path

--- scripts/build/postinstall.js
+++ scripts/build/postinstall.js
@@ -3,10 +3,10 @@ var cwd = process.cwd()
 var path = require('path')
 var exec = require('child_process').exec
 
-if (cwd.indexOf('node_modules') > -1) {
+if (cwd.indexOf('node_modules') === -1) {
   console.log('in node_modules context')
   // do stuff
-  var webpackPath = path.join(cwd, 'node_modules', '.bin', 'webpack')
+  var webpackPath = path.resolve(cwd, 'node_modules', '.bin', 'webpack')
   var command = webpackPath + ' --display-chunks --color --config webpack.config.components.babel.js'
   exec(command, {cwd: cwd}, function (error, stdout, stderr) {
     if (error) {
