38d6f82b7a24efe516dcdb595d37f133c91a18f2
davidwells <david@inboundnow.com>
2016-09-27 21:25:27 -0700

    update deps and add component build process

---
+++ scripts/components/post-build.js
@@ -0,0 +1,29 @@
+var fs = require('fs-extra')
+var path = require('path')
+var dir = require('node-dir')
+var componentsDir = path.join(__dirname, '..', '..', 'lib')
+
+function updateFileContents (filePath, callBack) {
+  dir.readFiles(filePath, {
+    match: /.js$/,
+    //exclude: /^\./
+  }, function (err, content, filename, next) {
+    if (err) throw err
+    console.log(filename)
+
+    var replace = content.replace(/\/\/ignoremodule.exports/g, 'module.exports')
+    fs.writeFileSync(filename, replace)
+
+    next()
+  },
+    function (err, files) {
+      if (err) {
+        callBack(err)
+      }
+      console.log('Components rdy to rock and roll')
+      callBack && callBack(null, files)
+    }
+  )
+}
+
+updateFileContents(componentsDir)
