b3be7b79b1b8e2d16df0555435fff519c60a8019
davidwells <david@inboundnow.com>
2016-09-28 16:17:44 -0700

    update check

--- scripts/build/postinstall.js
+++ scripts/build/postinstall.js
@@ -3,7 +3,7 @@ var cwd = process.cwd()
 var path = require('path')
 var exec = require('child_process').exec
 
-if (cwd.indexOf('node_modules') === -1) {
+if (cwd.indexOf('node_modules') > -1) {
   console.log('in node_modules context')
   // do stuff
   var webpackPath = path.resolve(cwd, 'node_modules', '.bin', 'webpack')
