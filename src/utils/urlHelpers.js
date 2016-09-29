09f731376466c7141077e0ac8f142ff35f04ea68
davidwells <david@inboundnow.com>
2016-09-28 21:06:54 -0700

    update params helper

--- src/utils/urlHelpers.js
+++ src/utils/urlHelpers.js
@@ -1,17 +1,18 @@
 
 export default function getURLParams (url) {
-  var urlParams = {}
-  var params
+  const urlParams = {}
+  const pattern = /([^&=]+)=?([^&]*)/g
+  let params
   var matches
-  var decode = function (s) {
-    return decodeURIComponent(s).replace(/\+/g, ' ')
-  }
+
   if (url) {
     params = (url.split('?').length <= 2) ? url.split('?')[1] : ''
   } else {
     params = window.location.search.substring(1)
   }
-  var pattern = /([^&=]+)=?([^&]*)/g
+  if (!params) {
+    return false
+  }
   while (matches = pattern.exec(params)) { // eslint-disable-line
     if (matches[1].indexOf('[') == '-1') { // eslint-disable-line
       urlParams[decode(matches[1])] = decode(matches[2])
@@ -35,6 +36,9 @@ export default function getURLParams (url) {
   return urlParams
 }
 
+function decode (s) {
+  return decodeURIComponent(s).replace(/\+/g, ' ')
+}
 /*
 var getParameterVal = function (name, string) {
   return (RegExp(name + '=' + '(.+?)(&|$)').exec(string) || [, false])[1]
