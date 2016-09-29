3058f3acefff90b6378260a01ad52827f4cacec7
davidwells <david@inboundnow.com>
2016-09-28 17:05:13 -0700

    update redirects

--- scripts/build/postinstall.js
+++ scripts/build/postinstall.js
@@ -2,21 +2,21 @@
 var cwd = process.cwd()
 var path = require('path')
 var exec = require('child_process').exec
-var lookupSync = require('climb-lookup').lookupSync
 
 if (cwd.indexOf('node_modules') > -1) {
   console.log('in node_modules context')
   // do stuff
-  var nodeModulesPath = lookupSync('node_modules')
-  var webpackPath = path.resolve(nodeModulesPath, '.bin', 'webpack')
-  var command = webpackPath + ' --display-chunks --color --config webpack.config.components.babel.js'
-  exec(command, {cwd: cwd}, function (error, stdout, stderr) {
-    if (error) {
-      console.warn(error)
-    }
-    console.log(stdout)
-    console.log('Built components for consumer app')
-  })
+  // var lookupSync = require('climb-lookup').lookupSync
+  // var nodeModulesPath = lookupSync('node_modules')
+  // var webpackPath = path.resolve(nodeModulesPath, '.bin', 'webpack')
+  // var command = webpackPath + ' --display-chunks --color --config webpack.config.components.babel.js'
+  // exec(command, {cwd: cwd}, function (error, stdout, stderr) {
+  //   if (error) {
+  //     console.warn(error)
+  //   }
+  //   console.log(stdout)
+  //   console.log('Built components for consumer app')
+  // })
 } else {
-  console.log('site')
+  console.log('in normal project site')
 }
