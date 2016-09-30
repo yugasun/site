af8664fe0c2090050a30a55542b7f386ef5ab3fc
davidwells <david@inboundnow.com>
2016-09-29 17:11:11 -0700

    add google tag for verification

--- src/fragments/HeadTag/HeadTag.js
+++ src/fragments/HeadTag/HeadTag.js
@@ -12,6 +12,10 @@ export default class HeadTag extends React.Component {
           meta={[
             { property: 'og:site_name', content: process.env.SITENAME },
             { name: 'twitter:site', content: `@${process.env.TWITTER}` },
+            {
+              name: 'google-site-verification',
+              content: '1HdFnJaYYEiONgCRl3fj7lQobYY4uXPe5L2-sTgNzKQ'
+            },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' }
           ]}
         />
