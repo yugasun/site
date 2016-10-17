43341c18b8433d74c68a385057cdf24112f59a5b
davidwells <david@inboundnow.com>
2016-10-16 17:40:12 -0700

    update event emitter auth

--- src/utils/auth/xsrfToken.js
+++ src/utils/auth/xsrfToken.js
@@ -20,23 +20,3 @@ export const initializeXsrfToken = () => {
     setXsrfToken()
   }
 }
-
-/*  const { authenticated, reason } = parseAuthHash(location.hash);
-export function parseAuthHash (hash) {
-  const authResult = new Auth0({
-    clientID: CLIENT_ID,
-    domain: DOMAIN,
-  }).parseHash(hash);
-
-  if (!authResult.idTokenPayload.email_verified) {
-    return { authenticated: false, reason: 'email-not-verified' };
-  }
-
-  if (authResult && authResult.idToken && authResult.state === getXsrfToken()) {
-    setAuthToken(authResult.idToken);
-    return { authenticated: true };
-  }
-
-  return { authenticated: false, reason: 'invalid-token' };
-}
-*/
