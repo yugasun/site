a4fc93ce6f8d11319bcc7bc54c16d9d93fe16221
davidwells <david@inboundnow.com>
2016-10-16 21:48:45 -0700

    update doc layout

--- src/layouts/Doc/index.js
+++ src/layouts/Doc/index.js
@@ -117,7 +117,7 @@ class Doc extends Component {
   }
   renderSidebar () {
     const childrenItems = this.renderChildrenList()
-    const parentItems = this.renderParentList()
+    // const parentItems = this.renderParentList()
     return (
       <div className={styles.sidebar + ' docs-sidebar'}>
         <div ref='sidebar' className={styles.sidebarInner}>
@@ -138,7 +138,6 @@ class Doc extends Component {
             </div>
           </div>
           {childrenItems}
-          {parentItems}
           <div className={styles.versionNumber}>
             Docs Version: {process.env.DOCS_VERSION}
           </div>
