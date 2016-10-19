a796d9e3e08eaf1bcb0c4726696b752263029a25
davidwells <david@inboundnow.com>
2016-10-19 16:39:05 -0700

    build files from /temp instead of node_modules

--- scripts/blog/config.js
+++ scripts/blog/config.js
@@ -5,8 +5,8 @@ var path = require('path')
 const repoURL = 'https://github.com/serverless/blog'
 module.exports = {
   repoURL: repoURL,
-  blogNodeModulesPath: path.join(__dirname, '..', '..', 'node_modules/serverless-blog'),
+  blogNodeModulesPath: path.join(__dirname, '..', '..', 'temp/serverless-blog'),
   newBlogPath: path.join(__dirname, '..', '..', 'content/blog/'),
-  oldBlogPath: path.join(__dirname, '..', '..', 'node_modules/serverless-blog/posts/'),
+  oldBlogPath: path.join(__dirname, '..', '..', 'temp/serverless-blog/posts/'),
   downloadLink: `${repoURL}/archive/master/.zip`
 }
--- scripts/docs/config.js
+++ scripts/docs/config.js
@@ -5,9 +5,9 @@ var path = require('path')
 const repoURL = 'https://github.com/serverless/serverless'
 module.exports = {
   repoURL: repoURL,
-  slsPath: path.join(__dirname, '..', '..', 'node_modules/serverless'),
+  slsPath: path.join(__dirname, '..', '..', 'temp/serverless'),
   newDocsPath: path.join(__dirname, '..', '..', 'content/framework/docs/'),
-  oldDocsPath: path.join(__dirname, '..', '..', 'node_modules/serverless/docs/'),
+  oldDocsPath: path.join(__dirname, '..', '..', 'temp/serverless/docs/'),
   docsMenuPath: path.join(__dirname, '..', '..', 'src/layouts/Doc/'),
   downloadLink: `${repoURL}/archive/master/.zip`
 }
