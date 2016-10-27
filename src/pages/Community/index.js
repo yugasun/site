003081cd4f0c6bdb48085c0ee8fa7c9ff9fb968d
davidwells <david@inboundnow.com>
2016-10-27 11:46:48 -0700

    update community page

--- src/pages/Community/index.js
+++ src/pages/Community/index.js
@@ -1,42 +1,13 @@
 import React, { Component, PropTypes } from 'react'
-import enhanceCollection from 'phenomic/lib/enhance-collection'
-import { addScript, removeScript } from '../../utils/manageScripts'
 import Default from '../../layouts/Default'
-import PagesList from '../../components/PagesList'
-
-const numberOfLatestPosts = 6
-const isClient = typeof window !== 'undefined'
 
 export default class CommunityPage extends Component {
   static contextTypes = {
     collection: PropTypes.array.isRequired,
   }
-  componentWillMount() {
-    if (isClient) {
-      addScript('//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js')
-    }
-  }
-  componentWillUnmount() {
-    console.log('unmount called on community')
-    if (isClient) {
-      removeScript('//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js')
-      delete window.jQuery
-    }
-  }
-
   render() {
-    const latestPosts = enhanceCollection(this.context.collection, {
-      filter: { layout: 'Post' },
-      sort: 'date',
-      reverse: true,
-    })
-    .slice(0, numberOfLatestPosts)
-
     return (
-      <Default {...this.props}>
-        <h2>{'Latest resources'}</h2>
-        <PagesList pages={latestPosts} />
-      </Default>
+      <Default {...this.props} />
     )
   }
 }
