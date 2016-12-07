db8f6e9c8a1fb4625c9fd24a50360005548ea4d6
davidwells <david@inboundnow.com>
2016-12-07 14:38:22 -0800

    add team page

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -27,6 +27,7 @@ export default class TeamPage extends React.Component {
     return (
       <Default {...this.props} className={styles.partnerPage}>
         <h1>The Serverless Team</h1>
+        <p>We're a passionate group of developers, writers, and operators who love what we do. Half of us work in the San Francisco office with the other half distributed across the globe. We get together twice a year at different exotic retreat locations to keep the good vibes flowing.</p>
         <section className={styles.tiles}>
           {this.listTeamMembers()}
         </section>
