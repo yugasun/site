405af6351d83d19137e5b4646eda74ee4deb3817
davidwells <david@inboundnow.com>
2017-01-25 14:35:26 -0800

    add referral util

--- src/utils/analytics/source/referrer.js
+++ src/utils/analytics/source/referrer.js
@@ -1,9 +1,22 @@
 /*
-const isRealReferrer = function () {
-  return document.referrer.split( '/' )[2] != site_hostname;
-}
-
-function removeProtocol (href) {
-  return href.replace(/.*?:\/\//g, "");
-}
+  Get referral data
 */
+const isExternalReferrer = () => {
+  if (document.referrer) {
+    const port = window.document.location.port
+    let ref = document.referrer.split('/')[2]
+    if (port) {
+      ref = ref.replace(`:${port}`, '')
+    }
+    // console.log('ref', ref)
+    // console.log('host', window.location.hostname)
+    return ref !== window.location.hostname
+  }
+  return false
+}
+/*
+function removeProtocol(href) {
+  return href.replace(/.*?:\/\//g, '')
+}*/
+
+export default isExternalReferrer
