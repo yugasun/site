101bc643463d0f81adc3b3c3d708efa476e2add5
davidwells <david@inboundnow.com>
2017-01-25 14:52:25 -0800

    sw test

--- src/app.js
+++ src/app.js
@@ -27,17 +27,17 @@ const propTypes = {
 
 /* eslint-disable */
 if(typeof window !== 'undefined') {
-;(function(history){
-    var pushState = history.pushState
-    history.pushState = function(state) {
-        if (typeof history.onpushstate === "function") {
-            history.onpushstate({state: state})
-        }
-        // whatever else you want to do
-        // maybe call onhashchange e.handler
-        return pushState.apply(history, arguments)
-    }
-})(window.history)
+  ;(function(history){
+      var pushState = history.pushState
+      history.pushState = function(state) {
+          if (typeof history.onpushstate === "function") {
+              history.onpushstate({state: state})
+          }
+          // whatever else you want to do
+          // maybe call onhashchange e.handler
+          return pushState.apply(history, arguments)
+      }
+  })(window.history)
 }
 
 function handleRouteChange(e) {
