bf62c67f9537308f1f73cf3335029e7602cd585a
davidwells <david@inboundnow.com>
2017-01-25 14:37:53 -0800

    add referral data

--- content/framework/status.md
+++ content/framework/status.md
@@ -5,7 +5,7 @@ fullWidth: true
 scripts: https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/apps/status-board/status-board-loader.js
 inlineCSS: "
   #footer { display: none; }
-  #status{ min-height: 100vh; background: #1a1a1a; }
+  #status-board { min-height: 100vh; background: #1a1a1a; }
   body { overflow: hidden; }
 "
 ---
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import Scripts from './fragments/GlobalScripts'
 import SubscribeModal from './fragments/SubscribeModal'
 import { initializeVisitorID } from './utils/analytics/visitor'
 import initUAClasses from './utils/brower-detect'
+import isExternalReferrer from './utils/analytics/source/referrer'
 /* Import global CSS before other components and their styles */
 import './index.global.css'
 import styles from './index.css'
@@ -24,12 +25,41 @@ const propTypes = {
   history: PropTypes.object,
 }
 
+/* eslint-disable */
+;(function(history){
+    var pushState = history.pushState
+    history.pushState = function(state) {
+        if (typeof history.onpushstate === "function") {
+            history.onpushstate({state: state})
+        }
+        // whatever else you want to do
+        // maybe call onhashchange e.handler
+        return pushState.apply(history, arguments)
+    }
+})(window.history);
+
+function handleRouteChange(e) {
+  const previousURL = window.location.href
+  setTimeout(() => {
+    const newURL = window.location.href
+    // console.log('previousURL', previousURL)
+    // console.log('newURL', newURL)
+  }, 0)
+}
+/* eslint-enable */
+
 export default class App extends Component {
   componentDidMount() {
     initializeVisitorID()
     window.addEventListener('reactRouterRedirect', this.handleAuthRedirect, false)
     // add browser based classes
     initUAClasses()
+    window.onpopstate = history.onpushstate = handleRouteChange
+    const externalReferrer = isExternalReferrer()
+    if (externalReferrer) {
+      console.log('Referring site', document.referrer) // eslint-disable-line
+      // console.log(document.referrer.split('/')[2])
+    }
   }
   componentWillUnmount() {
     window.removeEventListener('reactRouterRedirect', this.handleAuthRedirect)
