1067e1f0a0d12dec9fe8e1148c40cc5ad211362f
davidwells <david@inboundnow.com>
2017-02-02 12:18:11 -0800

    update UTM

--- src/utils/analytics/source/removeUTM.js
+++ src/utils/analytics/source/removeUTM.js
@@ -4,7 +4,7 @@
 const removeUTM = () => {
   if (typeof window !== 'undefined') {
     if (window.location.search.indexOf('utm_') !== -1 && window.history && history.replaceState) {
-      const cleanURL = window.location.toString().replace(/(\&|\?)utm([_a-z0-9=]+)/g, '')
+      const cleanURL = window.location.toString().replace(/(\&|\?)utm([_a-z0-9=\.]+)/g, '')
       history.replaceState({}, '', cleanURL)
     }
   }
