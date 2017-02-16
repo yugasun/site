a0faf57be2943ac0fd67c88b06a0610b5a0fe753
davidwells <david@inboundnow.com>
2017-02-15 18:07:10 -0800

    add script

--- src/fragments/GlobalScripts/GlobalScripts.js
+++ src/fragments/GlobalScripts/GlobalScripts.js
@@ -1,11 +1,24 @@
 import React, { PropTypes } from 'react'
+import Helmet from 'react-helmet'
 import GoogleAnalytics from './GoogleAnalytics'
 import SegmentIO from './SegmentIO'
 
 export default function GlobalScripts(props) {
+  let hubspotScript
+  if (process.env.NODE_ENV === 'production') {
+    hubspotScript = (
+      <Helmet
+        script={[{
+          src: 'https://js.hs-scripts.com/2901603.js',
+          type: 'text/javascript'
+        }]}
+      />
+    )
+  }
   return (
     <div>
       <GoogleAnalytics {...props} />
+      {hubspotScript}
       <div dangerouslySetInnerHTML={{ __html: SegmentIO }} />
     </div>
   )
