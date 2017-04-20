3e23f07dff1d6f27758b74bc5c62a5056ce4600e
davidwells <david@inboundnow.com>
2017-04-20 13:43:41 -0700

    remove segment key

--- src/_config.js
+++ src/_config.js
@@ -12,7 +12,7 @@ module.exports = function siteConfig(isProduction) {
       AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'qaNtBzG1CGp0vYzDGl9O32nsh5VNw20r',
       AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || 'serverlessqa.auth0.com',
       ALGOLIA_ADMIN: process.env.ALGOLIA_ADMIN || '1f21cf7b86876a3ac23e35b2657894aa',
-      SEGMENT_KEY: process.env.SEGMENT_KEY || 'gNftvcGllpKZTe5PcAxF8pEoenLq32H0',
+      // SEGMENT_KEY: process.env.SEGMENT_KEY || 'gNftvcGllpKZTe5PcAxF8pEoenLq32H0',
       CUSTOMER_IO_KEY: process.env.CUSTOMER_IO_KEY || '219fca0102ab3451a96a',
       /* Serverless API Gateway Endpoints */
       API: {
