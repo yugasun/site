b1472bc0a413cb13e8f7210cf95fbb64d0776344
davidwells <david@inboundnow.com>
2017-04-20 13:45:40 -0700

    update URL cleaning regex

--- src/utils/analytics/source/removeUTM.js
+++ src/utils/analytics/source/removeUTM.js
@@ -2,11 +2,15 @@
   Remove UTM tokens from URL
 */
 const removeUTM = () => {
-  if (typeof window !== 'undefined') {
-    if (window.location.search.indexOf('utm_') !== -1 && window.history && history.replaceState) {
-      const cleanURL = window.location.toString().replace(/(\&|\?)utm([_a-z0-9=\.]+)/g, '')
-      history.replaceState({}, '', cleanURL)
+  if (typeof window === 'undefined') {
+    return false
+  }
+  if (window.location.search.indexOf('utm_') !== -1 && window.history && history.replaceState) {
+    let url = window.location.toString().replace(/(\&|\?)utm([_a-zA-Z0-9=\.%]+)/g, '')
+    if (url.indexOf('&') > -1 && url.indexOf('?') === -1) {
+      url = url.replace(/&/, '?')
     }
+    history.replaceState({}, '', url)
   }
 }
 export default removeUTM
