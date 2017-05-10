f6debbc138832eb6ba4d5bf3df48c470d66e69ee
davidwells <david@inboundnow.com>
2017-05-09 19:08:28 -0700

    update name data

--- src/utils/analytics/identify.js
+++ src/utils/analytics/identify.js
@@ -5,33 +5,25 @@ export default function identify(id, profile) {
       id: id,
       email: profile.email, // Email of the currently signed in user.
       created_at: Math.round(+new Date(profile.created_at) / 1000),
+      // set name
+      ...getNameData(profile),
     }
     // limits https://learn.customer.io/api/#api-documentationlimits
     if (profile) {
-        // set name
-      if (profile.name) {
-        const name = profile.name.split(' ')
-        if (name[0]) {
-          data.first_name = name[0]
-        }
-        if (name[1]) {
-          data.last_name = name[1]
-        }
-      }
-        // set picture
+      // set picture
       if (profile.picture) {
         data.picture = profile.picture
       }
-        // set company
+      // set company
       const companyName = getCompanyName(profile)
       if (companyName) {
         data.company = companyName
       }
-        // set location
+      // set location
       if (profile.location) {
         data.location = profile.location
       }
-        // set login_count
+      // set login_count
       if (profile.login_count) {
         data.login_count = profile.login_count
       }
@@ -39,6 +31,40 @@ export default function identify(id, profile) {
     console.log('identify', data) // eslint-disable-line
      _cio.identify(data) // eslint-disable-line
   }
+}
+
+function getNameData(profile) {
+  if (!profile) {
+    return null
+  }
+  const nameData = {}
+
+  if (profile.name && typeof profile.name === 'string' && !profile.name.match(/@/)) {
+    const name = profile.name.split(' ')
+    if (name[0]) {
+      nameData.first_name = name[0]
+    }
+    if (name[1]) {
+      nameData.last_name = name[1]
+    }
+    if (name[0] && name[1]) {
+      nameData.full_name = `${name[0]} ${name[1]}`
+    }
+  }
+
+  const meta = profile.user_metadata
+  if (meta && meta.fullcontact && meta.fullcontact.contactInfo) {
+    if (meta.fullcontact.contactInfo.givenName) {
+      nameData.first_name = meta.fullcontact.contactInfo.givenName
+    }
+    if (meta.fullcontact.contactInfo.familyName) {
+      nameData.last_name = meta.fullcontact.contactInfo.familyName
+    }
+    if (meta.fullcontact.contactInfo.fullName) {
+      nameData.full_name = meta.fullcontact.contactInfo.fullName
+    }
+  }
+  return nameData
 }
 
 
