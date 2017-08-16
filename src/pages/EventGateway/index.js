7e0ddc53558bab5fb0b0936c086fa5e17a4ed6da
Andrea <30416413+andrea-p@users.noreply.github.com>
2017-08-15 17:10:44 -0700

    Update index.js

--- src/pages/EventGateway/index.js
+++ src/pages/EventGateway/index.js
@@ -10,7 +10,7 @@ const EventGatewayPage = (props) => {
         <div className={styles.boundSmaller}>
           <img className={styles.toolIcon} width={200} height={200} src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/gateway.svg' />
           <h1 className={styles.toolTitle}>Event Gateway</h1>
-          <p className={styles.toolDescription}>React to any event, with any function, on any provider. Let your application span the cloud.</p>
+          <p className={styles.toolDescription}>React to any event, with any function, on any provider. Break away from the single cloud experience.</p>
           <a className={`${styles.btn} ${styles.btnGrey}`} href='https://github.com/serverless/event-gateway'><img width={24} height={24} src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/images/github-icon.svg' /> Github</a>
         </div>
       </div>
@@ -21,6 +21,14 @@ const EventGatewayPage = (props) => {
             <p>A single piece of infrastructure for any type of serverless application.</p>
           </div>
           <div className={styles.feature}>
+            <h3 className={styles.featureTitle}>Extendable through middleware</h3>
+            <p>Perform data transforms, authorizations, serializations, and other custom computes straight from the Gateway.</p>
+          </div>
+          <div className={styles.feature}>
+            <h3 className={styles.featureTitle}>Platform agnostic</h3>
+            <p>All your cloud services are now compatible with one another: share cross-cloud functions and events with AWS Lambda, Microsoft Azure, IBM OpenWhisk and Google Cloud Platform.</p>
+          </div>
+          <div className={styles.feature}>
             <h3 className={styles.featureTitle}>Send events from any cloud</h3>
             <p>Data streams in your application become events. Centralize events from any cloud provider to get a bird’s eye view of all the data flowing through your cloud.</p>
           </div>
@@ -29,16 +37,8 @@ const EventGatewayPage = (props) => {
             <p>You aren’t locked in to events and functions being on the same provider: Any event, on any cloud, can trigger any function. Set events and functions up like dominoes and watch them fall.</p>
           </div>
           <div className={styles.feature}>
-            <h3 className={styles.featureTitle}>Extendable through middleware</h3>
-            <p>Perform data transforms, authorizations, serializations, and other custom computes straight from the Gateway.</p>
-          </div>
-          <div className={styles.feature}>
             <h3 className={styles.featureTitle}>Expose events to your team</h3>
             <p>Share events and functions to other parts of the application. Your teammates can find them and utilize them in their own services.</p>
-          </div>
-          <div className={styles.feature}>
-            <h3 className={styles.featureTitle}>Platform agnostic</h3>
-            <p>All your cloud services are now compatible with one another: share cross-cloud functions and events with AWS Lambda, Microsoft Azure, IBM OpenWhisk and Google Cloud Platform.</p>
           </div>
         </div>
       </div>
