ff97f6eb55c3a06bac06a8143f5fb90dab765c27
steelbrain <i@steelbrain.me>
2017-08-16 23:42:33 +0500

    Newsletter: Make responsive

--- src/fragments/NewsletterStrip/index.js
+++ src/fragments/NewsletterStrip/index.js
@@ -5,8 +5,10 @@ import styles from './NewsletterStrip.css'
 const NewsletterStrip = (props) => {
   return (
     <div className={styles.container}>
-      <img className={styles.logo} width={52} height={35} src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/logos/serverless-logo.svg' />
-      <p className={styles.description}>Join our newsletter and get the latest news about Serverless products and happenings. #nospamwepromise</p>
+      <div className={styles.descriptionBlock}>
+        <img className={styles.logo} width={52} height={35} src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/logos/serverless-logo.svg' />
+        <p className={styles.description}>Join our newsletter and get the latest news about Serverless products and happenings. #nospamwepromise</p>
+      </div>
       <Newsletter className={styles.newsletter} />
     </div>
   )
