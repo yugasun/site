212c90c27781d961346c3c280890051fa36d33e8
Andrea <30416413+andrea-p@users.noreply.github.com>
2017-08-15 17:13:21 -0700

    Update index.js

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -47,7 +47,7 @@ export default class Homepage extends Component {
                   <img className={styles.toolCardIcon} src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/gateway.svg' />
                 </div>
                 <h4 className={styles.toolCardTagline}>Centralize events & data</h4>
-                <p>React to any event, with any function, on any cloud. Expose events and functions across the team.</p>
+                <p>Span the cloud. React to any event, with any function, on any provider.</p>
               </div>
               <Link to='/event-gateway/'>Learn more</Link>
             </div>
