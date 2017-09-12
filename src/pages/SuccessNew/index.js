801c105e07863edfd7794a5b3b15bf99ea8fd535
davidwells <david@inboundnow.com>
2017-09-12 15:51:35 -0700

    dry run

--- src/pages/SuccessNew/index.js
+++ src/pages/SuccessNew/index.js
@@ -18,18 +18,24 @@ export default class CLISuccessNew extends React.Component {
       const email = decoded.email
       const name = decoded.name
       const createdAt = decoded.created_at
-      // ID call
-      identify(userID, {
+
+      console.log('IDENTITy Call')
+      console.log(userID)
+
+      const data = {
         created_at: createdAt,
         email: email,
         name: name,
         // login_count: params.c,
         // frameworkId: params.id
-      })
+      }
+      console.log('LOG DATA', data)
+      // ID call
+      // identify(userID, data)
       // Track Login Success
-      track('site:cli_loginCompleted', {
-        label: 'Framework Login Completed'
-      })
+      // track('site:cli_loginCompleted', {
+      //   label: 'Framework Login Completed'
+      // })
     }
   }
   render() {
