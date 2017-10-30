3b906a21550e0ae46489a714ffb328f7e713ce18
Raees Iqbal <10067728+elsteelbrain@users.noreply.github.com>
2017-10-31 03:08:35 +0500

    Revert "Workshops: No border bottom for last section"
    This reverts commit cbea86a91d403fd041ea5e70c102600c22d2b7a7.

--- src/pages/WorkShops/Section.js
+++ src/pages/WorkShops/Section.js
@@ -6,7 +6,7 @@ const WorkshopSection = (props) => {
   const imgSrc = props.backgroundImage || 'http://www.fillmurray.com/740/110'
   const text = props.text || 'Dates for this workshop have not been finalized. Join the waitlist to get notified as soon as the dates are set.'
   return (
-    <div className={styles.section} style={{ backgroundImage: `url(${imgSrc})` }}>
+    <div style={{ backgroundImage: `url(${imgSrc})` }}>
       <div className={styles.contentWrapper}>
         <h2 className={styles.cityName} style={{ backgroundImage: `url(${imgSrc})` }}>{props.city || ''}</h2>
         <p className={styles.description}>
