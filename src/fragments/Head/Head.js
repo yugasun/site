85af84bdb70dae94fa6b034555b6436ad0128912
Alex DeBrie <alexdebrie1@gmail.com>
2018-01-12 11:51:14 -0600

    Update Head.js

--- src/fragments/Head/Head.js
+++ src/fragments/Head/Head.js
@@ -14,7 +14,7 @@ const Head = (props) => {
           { property: 'og:site_name', content: process.env.SITENAME },
           { name: 'twitter:site', content: `@${process.env.TWITTER}` },
           { name: 'google-site-verification',
-            content: '1HdFnJaYYEiONgCRl3fj7lQobYY4uXPe5L2-sTgNzKQ'
+            content: '5deyORP0Li8BwxR57BPSdEKNWQVkejO_pFt_prXmvgI'
           },
           { name: 'viewport', content: 'width=device-width, initial-scale=1' }
         ]}
