ba7b45cc9b69371af78d7b83ef45df91b0072668
Kameshwaran <kamesh@codebrahma.com>
2018-07-16 01:36:37 +0530

    Fixed top space for pages

--- src/_variables.js
+++ src/_variables.js
@@ -7,7 +7,7 @@ module.exports = () => {
   return {
     // -- Sizing
     unit: '1rem',
-    navHeight: '8rem',
+    navHeight: '7.9rem',
     footerHeight: '15.5rem',
     // -- Colors
     yellow: '#ffd734',
