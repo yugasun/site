01e3ab9806cb26dad04104a0adc672eeee8ca974
Kameshwaran <kamesh@codebrahma.com>
2018-07-27 01:27:18 +0530

    Made trusted clients section as re-usable

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -23,11 +23,7 @@ import illustration1 from '../../assets/images/illustration-1.png'
 import illustration2 from '../../assets/images/illustration-2.png'
 import illustration3 from '../../assets/images/illustration-3.png'
 
-import ea from '../../assets/images/ea-logo.png'
-import cocacolaLogo from '../../assets/images/cocacola-logo.png'
-import nordstrom from '../../assets/images/nordstrom-logo.png'
-import expedia from '../../assets/images/expedia-logo.png'
-import reuters from '../../assets/images/reuters-logo.png'
+import TrustedClients from '../../fragments/TrustedClients';
 import PreFooter from '../../fragments/PreFooter';
 
 export default class Homepage extends Component {
@@ -206,18 +202,7 @@ export default class Homepage extends Component {
           </div>
         </div>
 
-        <div className={`${styles.clientsWrapper} ${styles.footerPrevPad} ${styles.patternSectionWrapper} ${styles.newSite}`}>
-          <div className={`${styles.clientContainer} ${commonStyles.container} ${styles.trustedByContainer}`}>
-            <h4>Serverless Platform is trusted by</h4>
-            <ul className={`${styles.clientList} ${styles.trustedBy}`}>
-              <li><img src={ea} alt='Electronic Arts' /></li>
-              <li><img src={cocacolaLogo} alt='Coca Cola' /></li>
-              <li><img src={nordstrom} alt='Nordstorm' /></li>
-              <li><img src={expedia} alt='Expedia' /></li>
-              <li><img src={reuters}  alt='Reuters' /></li>
-            </ul>
-          </div>
-        </div>
+        <TrustedClients />
       </Default>
     )
   }
