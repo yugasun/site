a3b7203ef5120af41516f339628c34f7c0989eb1
Kameshwaran <kamesh@codebrahma.com>
2018-07-26 22:35:07 +0530

    Added pre-footer

--- src/pages/Enterprise/index.js
+++ src/pages/Enterprise/index.js
@@ -1,125 +1,30 @@
 import React from 'react'
-import Form from '../../components/Form'
-import TextInput from '../../components/TextInput'
-import Checkbox from '../../components/Checkbox/Check'
-import Button from '../../components/Button'
-import track from '../../utils/analytics/track'
 import Default from '../../layouts/Default'
+import PreFooter from './../../fragments/PreFooter'
 import styles from './Enterprise.css'
-import formHandler from '../../utils/formHandler'
 import commonStyles from '../../fragments/common.css'
 import ContactForm from './contact-form'
 
 export default class PartnersPage extends React.Component {
   static hasLoadingState = true
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      loading: false,
-      error: false,
-      success: false,
-    }
+
+  renderPrefooter() {
+    return  (
+      <PreFooter
+        heading='New to serverless?'
+        descriptions={[
+          'To get started, pop open your terminal & run:',
+          'npm install serverless -g'
+        ]}
+        links={[
+          { url: '/framework/docs', title: 'documentation' },
+          { url: '/framework/docs/providers/aws/examples/', title: 'serverless example' },
+          { url: 'https://github.com/serverless/serverless', title: 'github repo' }
+        ]}
+      />
+    );
   }
-  onSubmit = (event, data) => {
-    event.preventDefault()
-    const formId = event.target.id
 
-    this.setState({
-      loading: true
-    })
-
-    const formData = {
-      formId: formId,
-      fields: data
-    }
-
-    // return false
-
-    formHandler(formData).then((res) => {
-      this.setState({
-        success: true,
-        loading: false
-      })
-    }).catch((e) => {
-      console.log('ERROR', e)
-      this.setState({
-        success: false,
-        loading: false,
-        error: e
-      })
-    })
-  }
-  renderForm() {
-    const { loading, success } = this.state
-    const formId = 'enterprise-contact-us'
-
-    if (success) {
-      track('site:enterprise_contact', {
-        category: 'Contact Form'});
-      return (
-        <div>
-          <p className={styles.thanks}>
-            Thanks we will be in touch shortly.
-          </p>
-        </div>
-      )
-
-    }
-
-    const buttonText = (loading) ? "LOADING" : "SUBMIT"
-
-    return (
-      <Form id={formId} onSubmit={this.onSubmit} onChange={this.onChange} trimOnSubmit>
-        <div className={styles.fieldSet}>
-          <h2 className={styles.header}>Let’s talk!</h2>
-          <div className={styles.inputs}>
-            <TextInput
-              name='name'
-              validation={'isFullName'}
-              placeholder='Name'
-              required
-            />
-            <TextInput
-              name='email'
-              validation={'isEmail'}
-              placeholder='Email'
-              errorMessageClassName={styles.errorMessage}
-              required
-            />
-            <TextInput
-              name='company'
-              placeholder='Company'
-              errorMessageClassName={styles.errorMessage}
-              required
-            />
-            <div className={styles.checkBox}>
-              <div className={styles.fakeLabel}>
-                Desired use case
-              </div>
-              <div className={styles.inputs}>
-                <Checkbox name={'poi'} label={'Standardizing Development'} />
-                <Checkbox name={'poi'} label={'Extending Legacy Infrastructure'} />
-                <Checkbox name={'poi'} label={'Multi-Cloud'} />
-                <Checkbox name={'poi'} label={'Other'} />
-              </div>
-        </div>
-            <div className={styles.inputs}>
-              <textarea
-                placeholder='Message'
-                className={styles.textArea}
-                name={'message'}
-               />
-            </div>
-          </div>
-        </div>
-        <div className={styles.submit}>
-          <Button kind={'red'} style={{padding: '13px 48px'}}>
-            {buttonText}
-          </Button>
-        </div>
-      </Form>
-    )
-  }
   render() {
     return (
       <Default
@@ -127,7 +32,8 @@ export default class PartnersPage extends React.Component {
         {...this.props}
         fullWidth
         whiteLogo={true}
-        coloredHeader={false}>
+        coloredHeader={false}
+        renderPrefooter={this.renderPrefooter}>
         <div className={`${commonStyles.container} ${styles.container}`}>
           <div className={`${styles.section} ${styles.contentSection}`}>
             <div className={styles.titleSection}>
