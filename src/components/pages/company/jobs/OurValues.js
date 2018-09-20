3d9f561ff6a5d1f8ccc7fa4e9498cad3dcf19ef7
Nick Gottlieb <ngottlieb1@gmail.com>
2018-09-19 18:38:39 -0700

    Update OurValues.js

--- src/components/pages/company/jobs/OurValues.js
+++ src/components/pages/company/jobs/OurValues.js
@@ -15,7 +15,7 @@ import { TitleWithIcon } from 'src/fragments'
 const ourValuesContent = [
   { header: 'Radical Candor', content: 'We have the courage to give and receive constructive feedback that challenges us to do better, but also shows that we care.' },
   { header: 'Freedom with accountability', content: 'We thrive in a state of freedom and autonomy. We own our outcomes, share the credit, and shoulder the responsibility.' },
-  { header: 'Futurew minded creativity', content: 'We make forward-thinking decisions for the long-term, not based on what others are doing right now. We identify root causes instead of treating symptoms.' },
+  { header: 'Future minded creativity', content: 'We make forward-thinking decisions for the long-term, not based on what others are doing right now. We identify root causes instead of treating symptoms.' },
   { header: 'Integrity', content: 'We treat everyone with respect. Our team is incredibly diverse: in nationality, country of residence, and background, and we believe this diversity makes our team better. More than that, it’s necessary for our team to do the creative thinking required to be a market leader.”' },
 ]
 
