98f482ed2ba5aa5725fcf375143bca5a44b97fc8
Kameshwaran <kamesh@codebrahma.com>
2018-09-25 12:21:47 +0530

    Fixed doc update

--- dynamic-content-generator/page/index.js
+++ dynamic-content-generator/page/index.js
@@ -2,10 +2,10 @@ const blogPagesGenerator = require('./blog')
 const docsPagesGenerator = require('./docs')
 const workshopPagesGenerator = require('./workshop')
 
-const pageCreator = (graphql, createPage) => (
+const pageCreator = (graphql, createPage, createRedirect) => (
   Promise.all([
     blogPagesGenerator(graphql, createPage),
-    docsPagesGenerator(graphql, createPage),
+    docsPagesGenerator(graphql, createPage, createRedirect),
     workshopPagesGenerator(createPage),
   ])
 )
