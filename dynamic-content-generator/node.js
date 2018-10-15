1c1ad6e24e39a4935ee159d1ac9690f323b8b3ea
Pravin <pdaryani@gmail.com>
2018-10-15 18:13:26 +0200

    guides/dynamodb.md file + function to source .md files from content/guides folder added

--- dynamic-content-generator/node.js
+++ dynamic-content-generator/node.js
@@ -10,8 +10,10 @@ const highlight = require('remark-highlight.js')
 const html = require('remark-html')
 const algoliasearch = require('algoliasearch')
 
+//TODO : Refactor to use filesystem/remark to do this + refactor to avoid repetition
 const blogConfig = require('./../scripts/blog/config')
 const docsConfig = require('./../scripts/docs/config')
+const guidesConfig = require('./../scripts/guides/config')
 
 const fileReadingOptions = { match: /.md$/ }
 
@@ -21,6 +23,33 @@ const index = client.initIndex(process.env.GATSBY_ALGOLIA_BLOG_INDEX)
 const digestCreator = (content) => (
   crypto.createHash(`md5`).update(JSON.stringify(content)).digest(`hex`)
 )
+
+const sourceGuides = (createNode) => (err, content, filename, next) => {
+  if (err) throw err
+
+  const { data, content: markdownContent } = matter(content)
+  const frontmatter = data.category ? data : { ...data, category: [] }
+  const guideId = path.basename(filename, path.extname(filename))
+
+  unified().
+    use(markdown).
+    use(highlight).
+    use(html).
+    process(markdownContent, (err, file) => {
+      createNode({
+        id: guideId,
+        parent: null,
+        children: [],
+        internal: {
+          type: 'Guide',
+          contentDigest: digestCreator(content),
+        },
+        frontmatter,
+        content: String(file),
+      })
+      next()
+    })
+}
 
 const sourceBlogs = (createNode) => (err, content, filename, next) => {
   if (err) throw err
@@ -98,6 +127,15 @@ const generator = (createNode) => (
         blogConfig.siteBlogPath,
         fileReadingOptions,
         sourceBlogs(createNode),
+        resolve
+      )
+    }),
+
+    new Promise((resolve, reject) => {
+      dir.readFiles(
+        guidesConfig.siteGuidePath,
+        fileReadingOptions,
+        sourceGuides(createNode),
         resolve
       )
     }),
