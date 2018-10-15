315f06b3d5baa8efed4c95d12e58879cf1ffb779
Pravin <pdaryani@gmail.com>
2018-10-15 22:42:36 +0200

    fix for rendering HTML - using Box instead of P

--- src/pages/developers/guides/dynamodb.js
+++ src/pages/developers/guides/dynamodb.js
@@ -2,10 +2,13 @@ import React from 'react'
 import DefaultLayout from 'src/layouts/Default'
 import { Helmet } from 'src/fragments'
 import { NewToServerlessPrefooter } from 'src/fragments'
-import {Box, Heading, P} from 'serverless-design-system'
+import {Box, Heading} from 'serverless-design-system'
 import styled from 'styled-components'
 
-const TextWithHoverableLinks = styled(P)`
+const TextWithHoverableLinks = styled(Box)`
+    font-family: "Soleil";
+    line-height: 1.63;
+    color: #5b5b5b;
     a:hover  {
         color: #5b5b5b;
     }
@@ -18,7 +21,7 @@ export default ({ data }) => (
         px={[30, 30, 300]}
         my={10}
         pb={200}>
-            <Heading.h1 align='center' fontSize={['32px', '32px', '48px']}>
+            <Heading.h1 align='center' fontSize={['32px', '32px', '48px']} fontFamily='SoleilBk'>
                 {data.Dynamo.frontmatter.title}
             </Heading.h1>
             <TextWithHoverableLinks width={1} pt={40} dangerouslySetInnerHTML={{ __html: data.Dynamo.content }} />
