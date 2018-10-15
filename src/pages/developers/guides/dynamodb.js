9035427c2a86844d59f7e5bb16f0d8fd10a1204f
Pravin <pdaryani@gmail.com>
2018-10-15 23:04:12 +0200

    fix for mobile view implemented

--- src/pages/developers/guides/dynamodb.js
+++ src/pages/developers/guides/dynamodb.js
@@ -2,30 +2,38 @@ import React from 'react'
 import DefaultLayout from 'src/layouts/Default'
 import { Helmet } from 'src/fragments'
 import { NewToServerlessPrefooter } from 'src/fragments'
-import {Box, Heading} from 'serverless-design-system'
+import {Box, Heading, Column} from 'serverless-design-system'
 import styled from 'styled-components'
 
-const TextWithHoverableLinks = styled(Box)`
+const PageContent = styled(Box)`
     font-family: "Soleil";
     line-height: 1.63;
     color: #5b5b5b;
     a:hover  {
         color: #5b5b5b;
     }
+
+    @media screen and (max-width: 414px) {
+        table {
+            display: none;
+        }
+    }
 `
 
 export default ({ data }) => (
   <DefaultLayout prefooter={NewToServerlessPrefooter} footerBackground={false}>
    <Helmet {...data.Dynamo.frontmatter} />
-   <Box
-        px={[30, 30, 300]}
-        my={10}
-        pb={200}>
-            <Heading.h1 align='center' fontSize={['32px', '32px', '48px']} fontFamily='SoleilBk'>
-                {data.Dynamo.frontmatter.title}
-            </Heading.h1>
-            <TextWithHoverableLinks width={1} pt={40} dangerouslySetInnerHTML={{ __html: data.Dynamo.content }} />
+   <Column width={1}>
+        <Box width={1}
+            px={[30, 30, 300]}
+            my={10}
+            pb={200}>
+                <Heading.h1 align='center' fontSize={['32px', '32px', '48px']} fontFamily='SoleilBk'>
+                    {data.Dynamo.frontmatter.title}
+                </Heading.h1>
+                <PageContent width={1} pt={40} mx='auto' dangerouslySetInnerHTML={{ __html: data.Dynamo.content }} />
         </Box>
+    </Column>
   </DefaultLayout>
 )
 
