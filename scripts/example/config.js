a2c9d64ace004f6f3b0cac9a55c342a76a206116
Pravin <pdaryani@gmail.com>
2018-10-20 17:20:01 +0200

    script added to automate serverless/examples data ingestion

--- scripts/example/config.js
+++ scripts/example/config.js
@@ -1,6 +1,36 @@
+/**
+  Constants used for blog processing scripts
+*/
 const path = require('path')
 const projectRoot = path.join(__dirname, '..', '..')
+const repoURL = 'https://github.com/pdaryani/examples' //TODO: change this
+const repoBranch = 'master'
+const featuredExamples = [ 
+'aws-java-simple-http-endpoint',
+'aws-node-alexa-skill',
+'aws-node-auth0-cognito-custom-authorizers-api',
+'aws-node-auth0-custom-authorizers-api',
+'aws-node-dynamodb-backup',
+'aws-node-env-variables-encrypted-in-a-file',
+'aws-node-env-variables',
+'aws-node-fetch-file-and-store-in-s3',
+'aws-node-function-compiled-with-babel',
+'aws-node-github-webhook-listener',
+'aws-node-graphql-api-with-dynamodb',
+'aws-node-iot-event',
+'aws-node-oauth-dropbox-api',
+'aws-node-recursive-function',
+'aws-node-rekognition-analysis-s3-image' 
+]
+const allPlatforms = []
+const languages = []
 
 module.exports = {
-    siteExamplePath: path.join(projectRoot, 'content/example/')
+  repoURL,
+  repoBranch,
+  featuredExamples,
+  downloadLink: `${repoURL}/archive/frontmatter-content-update/.zip`, //TODO: change this
+  examplesRepoPath: path.join(projectRoot, 'serverless-examples'),
+  examplesTmpPath: path.join(projectRoot, 'content/examples'),
+  siteExamplePath: path.join(projectRoot, 'content/examples/'),
 }
