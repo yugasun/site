0839b348d3a74d1e9db821e77436f5dbacea9c0c
Pravin <pdaryani@gmail.com>
2018-10-21 14:47:57 +0200

    newHeader seperated as a new component

---
+++ src/components/HeaderNew/Wrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { Overflow } from 'serverless-design-system'
+
+export default styled(Overflow)`
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
+`
