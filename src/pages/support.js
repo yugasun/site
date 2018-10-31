7b6fe011de36d1404df6e4880993abd57deba206
Pravin <pdaryani@gmail.com>
2018-10-31 18:32:46 +0100

    support page created

--- src/pages/support.js
+++ src/pages/support.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { TrustedClientsNew as TrustedClients, Helmet, NewToServerlessPrefooterNew as NewToServerlessPrefooter} from 'src/fragments'
 import DefaultLayout from 'src/layouts/DefaultNew'
-import Hero from 'src/components/pages/platform/Hero'
-import Content from 'src/components/pages/platform/Content'
+import Hero from 'src/components/pages/support/Hero'
+import Content from 'src/components/pages/support/Content'
 
 const Home = ({ location }) => {
   return (
