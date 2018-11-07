198d88c6963039a25d2582f2dba241fc3bd5f352
Pravin <pdaryani@gmail.com>
2018-11-07 19:47:07 +0100

    upcomingMeetups data.js file fix

--- src/components/pages/resources/events/UpcomingMeetups/data.js
+++ src/components/pages/resources/events/UpcomingMeetups/data.js
@@ -1,12 +1,4 @@
-import React from 'react'
-import {
-  Column,
-  ResponsiveStack,
-} from 'serverless-design-system'
-import { AppContainer } from 'src/components'
-import { MeetupCard, TitleWithIcon } from 'src/fragments'
-
-const meetupData = [
+export default [ 
     {
       title: 'Serverless Austin: Google Presents Serverless Computing',
       schedule: {
@@ -142,31 +134,4 @@ const meetupData = [
           plusCount:4,
         }
     },
-  ]
-const UpcomingMeetups = () => (
-  <AppContainer>
-    <Column alignItems='center' py={[2, 2, 3, 5]}>
-      <TitleWithIcon fontSize={[4, 4, 6, 7]}>
-        Upcoming Events
-      </TitleWithIcon>
-      <ResponsiveStack
-        flexWrap='wrap'
-        justifyContent='center'
-      >
-        {
-          meetupData.map(({ title, schedule, address, attending}) => (
-            <MeetupCard
-              key={title}
-              title={title}
-              schedule={schedule}
-              address={address}
-              attending={attending}
-            />
-          ))
-        }
-      </ResponsiveStack>
-    </Column>
-  </AppContainer>
-)
-
-export default UpcomingMeetups
+]
