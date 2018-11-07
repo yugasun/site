154fefdfca6efd5d4b22a70ef97e1d0363b774a9
Pravin <pdaryani@gmail.com>
2018-11-07 11:52:50 +0100

    oldFooter updated with new menu items

--- src/components/Footer/LeftSection/index.js
+++ src/components/Footer/LeftSection/index.js
@@ -3,21 +3,23 @@ import React from 'react'
 import { Flex, Column } from 'serverless-design-system'
 
 import CompanyDetails from './CompanyDetails'
-import PlatformColumn from './PlatformColumn'
+import ProductsColumn from './ProductsColumn'
 import DevelopersColumn from './DevelopersColumn'
 import LearnColumn from './LearnColumn'
-import ResourcesColumn from './ResourcesColumn'
+import CommunityColumn from './CommunityColumn'
 import CompanyColumn from './CompanyColumn'
+import StandaloneColumn from './StandaloneColumn'
 
 // Renders the entire footer list items
 const LeftSection = () => (
   <Column width={[1, 1, 0.65]} order={['2', '2', '1']}>
     <Flex width={1} flexWrap='wrap' px={0} mb={4}>
-      <PlatformColumn />
+      <ProductsColumn />
       <DevelopersColumn />
+      <CommunityColumn />
       <LearnColumn />
-      <ResourcesColumn />
       <CompanyColumn />
+      <StandaloneColumn />
     </Flex>
     <CompanyDetails />
   </Column>
