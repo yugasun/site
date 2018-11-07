74e43090cef99ab1273ae43ea0125a237af55e1c
Pravin <pdaryani@gmail.com>
2018-11-07 13:18:25 +0100

    oldHeader files deleted

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Header } from './Header'
 export { default as Footer } from './Footer'
 export { default as AppContainer } from './AppContainer'
 export { default as NavLink } from './NavLink'
