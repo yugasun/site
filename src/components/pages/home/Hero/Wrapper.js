63735a3a0b6f6eecb1bb51f89974265f54b25b80
Pravin <pdaryani@gmail.com>
2018-11-08 10:43:29 +0100

    custom heroWrapper for homepage

---
+++ src/components/pages/home/Hero/Wrapper.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { withTheme } from 'styled-components'
+import { Background, Box } from 'serverless-design-system'
+
+import { AppContainerNew as AppContainer } from 'src/components'
+
+const HeroWrapper = ({ children, theme, ...otherProps }) => (
+  <Background
+    {...otherProps}
+  >
+    <Box
+      width={1}
+      pt={[
+        theme.navbarHeights.mobile,
+        theme.navbarHeights.mobile,
+        theme.navbarHeights.desktop,
+      ]}
+    >
+      <AppContainer>
+        <Box px={[15, 15, 0]} py={['14px', '14px']}>
+          {children}
+        </Box>
+      </AppContainer>
+    </Box>
+  </Background>
+)
+
+export default withTheme(HeroWrapper)
