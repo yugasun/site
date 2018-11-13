ca2798d968bdea0a8008fc88b7056c6283c8e557
Daniel Schep <dschep@gmail.com>
2018-11-13 09:18:55 -0500

    build docs for current release
    1st, switched back to main repo for docs instead of fork.
    2nd, building the latest released tag instead of master. This is
    a good idea because it will avoid us getting issues like
    serverless/serverless#5479

--- scripts/docs/config.js
+++ scripts/docs/config.js
@@ -3,9 +3,9 @@
 */
 const path = require('path')
 const projectRoot = path.join(__dirname, '..', '..')
-const repoURL = 'https://github.com/pdaryani/serverless' //TODO: revert to serverless/serverless
-// set branch of docs you want to see
-const repoBranch = 'doc-build-error-fix' //TODO: revert to master
+const repoURL = 'https://github.com/serverless/serverless'
+// set branch of docs you want to see (set to tag of latest release)
+const repoBranch = 'v1.32.0'
 /* uncomment out the line below to work locally with different docs branch
 repoBranch = 'improve-docs'
 /**/
