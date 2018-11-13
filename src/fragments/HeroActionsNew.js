47007f818c23d4edf88f4106e46f616676cfa390
Pravin <pdaryani@gmail.com>
2018-11-13 11:37:57 +0100

    extended HeroActionsNew with justifyContent prop

--- src/fragments/HeroActionsNew.js
+++ src/fragments/HeroActionsNew.js
@@ -21,8 +21,9 @@ const HeroActions = ({
     crossDomain: scrossDomain = false,
     completed: sCompleted = false,
   },
+  justifyContent
 }) => (
-    <ResponsiveStack width={[1, 1, 1]} mx={'auto'} mt={3} mb={[0,0,3]}>
+    <ResponsiveStack width={[1, 1, 1]} mx={'auto'} mt={3} mb={[0,0,3]} justifyContent={justifyContent ? justifyContent : ''}>
     <NavLink to={firstLink} completed={fCompleted} crossDomain={fcrossDomain}>
       <Button mr={'32px'} my={1} border={'2.5px solid'} p={1}>
         {fName}
