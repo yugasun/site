163efa6ada61ac9f3e5b17484b11314a7c671b75
Pravin <pdaryani@gmail.com>
2018-11-28 10:59:17 +0100

    added H2/H5 to new design system

--- src/fragments/DesignSystem/Text/Heading/index.js
+++ src/fragments/DesignSystem/Text/Heading/index.js
@@ -1,9 +1,13 @@
 import h1 from './H1'
+import h2 from './H2'
 import h3 from './H3'
 import h4 from './H4'
+import h5 from './H5'
 
 export default {
     h1,
+    h2,
     h3,
-    h4
+    h4,
+    h5
 }
