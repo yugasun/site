398fe3306cc5624fa20b6d02023b73837e92f418
Pravin <pdaryani@gmail.com>
2018-11-30 19:27:16 +0100

    blog header logo link corrected

--- src/components/BlogHeader/Logo.js
+++ src/components/BlogHeader/Logo.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { push } from 'gatsby-link'
 import { Logo } from 'serverless-design-system'
 import logo from 'src/assets/images/blog-logo.svg'
-
+import { InternalLink } from 'src/fragments'
+import { resources } from 'src/constants/urls'
 const LogoComponent = () => (
-  <Logo
-    src={logo}
-    height={['20px', '20px', '20px', '30px']}
-    width={['150px', '150px', '150px', '170px']}
-    alt='Serverless Blog'
-    onClick={() => push('/blog')}
-  />
+  <InternalLink to={resources.blog}>
+    <Logo
+      src={logo}
+      height={['20px', '20px', '20px', '30px']}
+      width={['150px', '150px', '150px', '170px']}
+      alt='Serverless Blog'
+    />
+  </InternalLink>
 )
 
 export default LogoComponent
