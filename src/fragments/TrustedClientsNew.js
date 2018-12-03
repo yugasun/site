80677f3d2852a3d10ca3f32efd6f1ff130576803
Pravin <pdaryani@gmail.com>
2018-12-03 09:10:40 +0100

    updated Testimonial and TrustedClients for laptop views

--- src/fragments/TrustedClientsNew.js
+++ src/fragments/TrustedClientsNew.js
@@ -5,11 +5,10 @@ import {
   Box,
   Container,
   Heading,
-  ImageList,
   Flex,
   Image
 } from 'serverless-design-system'
-
+import { AppContainerNewest as AppContainer } from 'src/components'
 import eaLogo from 'src/assets/images/fragments/trusted-clients/ea-logo-gray.png'
 import cocacolaLogo from 'src/assets/images/fragments/trusted-clients/cocacola-logo-gray.png'
 import nordstromLogo from 'src/assets/images/fragments/trusted-clients/nordstrom-logo-gray.png'
@@ -18,22 +17,13 @@ import reutersLogo from 'src/assets/images/fragments/trusted-clients/reuters-log
 
 import backgroundImage from 'src/assets/images/rectangle-19.png'
 
-const TrustedClientList = [
-  { src: eaLogo, altText: 'EA' },
-  { src: cocacolaLogo, altText: 'Coca Cola' },
-  { src: nordstromLogo, altText: 'NordStrom' },
-  { src: expediaLogo, altText: 'Expedia' },
-  { src: reutersLogo, altText: 'Reuters' },
-]
-
 const TrustedClients = () => (
   <Background backgroundImage={[`url(${backgroundImage})`]} height={['700px', '700px', '619px']}>
-    <Container>
+    <AppContainer>
       <Box
         width={1}
         pt={['62px', '62px', '92px']}
         pb={[300, 300, 12, 12]}
-        px={[30, 30, 2, 6, 8]}
       >
         <Heading.h5
           align='center'
@@ -52,8 +42,8 @@ const TrustedClients = () => (
       flexWrap='wrap'
       mb={8}
     >
-    <Box mr={['20px', '20px', '20px', '20px', '0px']}>
-      <Image src={eaLogo} width={['54px', '54px', '54px', '54px', '90px']} height={['26px', '26px', '26px', '26px', '44px']} />
+    <Box mr={['20px', '20px', '20px', '20px', '0px']} ml={[0, 0, '50px', '50px', '100px']}>
+      <Image src={eaLogo} width={['54px', '54px', '54px', '54px', '72px', '90px']} height={['26px', '26px', '26px', '26px', '44px']} />
     </Box>
     
     <Box mr={['20px', '20px', '20px', '20px', '0px']}>
@@ -66,13 +56,13 @@ const TrustedClients = () => (
       <Image src={expediaLogo} width={['88px', '88px', '88px', '88px', '147px']} height={['25px', '25px', '25px', '25px', '42px']}/>
     </Box>
 
-    <Box mt={['20px', '20px', '0px']}>
+    <Box mt={['20px', '20px', '0px']} mr={[0, 0, '50px', '50px', '100px']}>
       <Image src={reutersLogo} width={['96px', '96px', '96px', '96px', '160px']} height={['25px', '25px', '25px', '25px', '42px']}/>
     </Box>
     
     </Flex>
       </Box>
-    </Container>
+    </AppContainer>
   </Background>
 )
 
