e6df4e010b7f2fb4e719fe5239db78bdb6f1b686
Pravin <pdaryani@gmail.com>
2018-12-07 21:27:21 +0100

    updated white button component defaults

--- src/components/WhiteButtonNew/index.js
+++ src/components/WhiteButtonNew/index.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { withTheme } from 'styled-components'
 import { SecondaryButton } from 'src/fragments/DesignSystem'
 
-const comp = ({ children, theme }) => (
+const comp = ({ children, theme, ...otherProps }) => (
   <SecondaryButton
     borderWidth='2px'
     color='white'
     p={0}
     height='36px'
-    letterSpacing={'0.8px'}
-    lineHeight='21px'
+    letterSpacing={'1.5px'}
+    lineHeight='24px'
+    {...otherProps}
     theme={{
       colors: {
         primaryColor: theme.colors.white,
         secondaryColor: theme.colors.white,
       },
     }}
+    
   >
     {children}
   </SecondaryButton>
