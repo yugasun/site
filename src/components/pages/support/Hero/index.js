120e36dc43346a7143368a68aa73af0150980acd
Pravin <pdaryani@gmail.com>
2018-12-14 21:07:54 +0100

    updated support page copy + updated style accordingly

--- src/components/pages/support/Hero/index.js
+++ src/components/pages/support/Hero/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Box, Image, Flex, Column } from 'serverless-design-system'
+import { Box, Image, Flex, Column, Heading } from 'serverless-design-system'
 import { HeroWrapperNew as HeroWrapper } from 'src/fragments'
 import serverlessPlatform from 'src/assets/images/pages/support/serverless-support.svg'
 import { Description, MainHeading, TopHeading } from 'src/components/pages/form-pages/Hero'
@@ -9,24 +9,26 @@ const ExamplesHero = () => {
   return (
   <HeroWrapper
     background={['linear-gradient(150deg,rgb(0,0,0) 50%,#70221f)', 'linear-gradient(150deg,rgb(0,0,0) 50%,#70221f)',`linear-gradient(132deg,rgb(0,0,0) 70%,#70221f)`]}
-    height={[420, 420, 500, 500, 614]}
+    height={[550, 550, 600, 500, 614]}
   >
 
     <Column
         color='white'
     >
-      <Box width={['auto', 'auto', 'auto', 'auto', 488]} mt={[0, 0, 0, 0, 150]}>
+      <Box width={['auto', 'auto', 'auto', 'auto', 438, 488]} mt={[0, 0, 0, 0, 30]}>
         <Flex flexDirection={['column', 'column', 'column', 'column','row']} alignItems={['center', 'center', 'center', 'center', 'left']}>
           <Box mt={[0, 0, 0, 0, '30px']}>
             <Image src={serverlessPlatform} height={'92px'} width={'92px'} />
           </Box>
-            <Column pl={['10px', '10px','10px', '10px', '30px']}>
+            <Column pl={['10px', '10px','10px', '10px', '36px']}>
               <TopHeading>serverless</TopHeading>
-              <MainHeading>support</MainHeading>
+              <Heading.h1 fontSize={['48px', '48px', '81px', '81px', '61px', '70px']} letterSpacing={'-1.56px'} lineHeight={['58px', '58px', '88px', '88px', '78px']} mb={0} align={['center', 'center', 'center', 'center', 'left']}>
+              support &amp; training
+              </Heading.h1>
           </Column>
         </Flex>
         <Description>
-          Enable your team to scale safely with best-in-class Serverless Support.
+        Eliminate the guesswork with dedicated enterprise support, training, and architecture design for your serverless application development.
         </Description>        
       </Box>
     </Column>
