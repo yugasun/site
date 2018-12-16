422a4d7dc86d840cf0d294784ab8e5f711148be5
Pravin <pdaryani@gmail.com>
2018-12-16 15:34:30 +0100

    auto-generating /kickstart pages

--- dynamic-content-generator/page/index.js
+++ dynamic-content-generator/page/index.js
@@ -2,11 +2,13 @@ const blogPagesGenerator = require('./blog')
 const docsPagesGenerator = require('./docs')
 const examplePagesGenerator = require('./example')
 const workshopPagesGenerator = require('./workshop')
+const kickstartPagesGenerator = require('./kickstart')
 
 const pageCreator = (graphql, createPage, createRedirect) => (
   Promise.all([
     blogPagesGenerator(graphql, createPage),
     examplePagesGenerator(graphql, createPage),
+    kickstartPagesGenerator(graphql, createPage),
     docsPagesGenerator(graphql, createPage, createRedirect),
     workshopPagesGenerator(createPage),
   ])
