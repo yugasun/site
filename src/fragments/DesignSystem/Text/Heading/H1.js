b85a4f2485f657055ca89bfedb9429e57fbbf98a
Pravin <pdaryani@gmail.com>
2018-12-31 15:59:05 +0100

    updated H1 mobile sizing in DesignSystem

--- src/fragments/DesignSystem/Text/Heading/H1.js
+++ src/fragments/DesignSystem/Text/Heading/H1.js
@@ -3,10 +3,10 @@ import { Heading } from 'serverless-design-system'
 
 const H1 = ({children, ...otherProps}) => (
     <Heading.h1
-      fontSize={['32px', '32px', '32px', '32px', '48px']}
+      fontSize={['24px', '24px', '32px', '32px', '48px']}
       fontFamily={'Soleil'}
-      lineHeight={['44px', '44px', '44px', '44px', '62px']}
-      letterSpacing={[0, 0, '0.5px']}
+      lineHeight={['38px', '38px', '44px', '44px', '62px']}
+      letterSpacing={['-0.38px', '-0.38px', '0.5px']}
       {...otherProps}
     >
       {children}
