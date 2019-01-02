25c8aa0af835f73483e3c98c654c89ab2094b7bd
Pravin <pdaryani@gmail.com>
2019-01-02 08:48:13 +0100

    blog author page - improved mobile spacing for blogs preview

--- src/components/pages/author/Blogs/Preview.js
+++ src/components/pages/author/Blogs/Preview.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Box } from 'serverless-design-system'
-import { AppContainer } from 'src/components'
 import BlogPreview from './SingleBlogPreview'
 
 const AuthorBlogsPreview = ({ blogs }) => (
-  <AppContainer>
     <Box
       width={[1, 1, 1, 1]}
-      mx='auto'
-      mt={[90]}
+      px={['auto', 'auto', 'auto', 'auto', 15]}
+      mt={[0, 0, 0, 0, 55]}
       mb={[32]}
     >
       { blogs.map((blog, index) => (<BlogPreview key={`blog-${index}`} {...blog} />)) }
     </Box>
-  </AppContainer>
 )
 
 export default AuthorBlogsPreview
