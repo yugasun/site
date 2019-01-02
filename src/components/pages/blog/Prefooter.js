7f4d9573dae762d86de681da8d500ef76870105b
Pravin <pdaryani@gmail.com>
2019-01-02 09:06:21 +0100

    removed blog mobile prefooter + changed 'sign up' to 'subscribe'

--- src/components/pages/blog/Prefooter.js
+++ src/components/pages/blog/Prefooter.js
@@ -9,17 +9,22 @@ import {
 } from 'serverless-design-system'
 import { NewsLetterForm } from 'src/components'
 import { Button } from 'src/fragments/DesignSystem'
+import styled from 'styled-components'
+
+const RelativeWithBoxShadow = styled(Relative)`
+  box-shadow: 2px 20px 50px 2px rgba(0, 0, 0, 0.15);
+`
 
 const SubmitButton = (props) => (
   <Button
-    width={0.4}
+    width={[1, 1, 1, 1, 0.4]}
     height={36}
     fontSize={1}
-    color='#fd5750'
-    bg='white'
+    color={['white', 'white', 'white', 'white', '#fd5750']}
+    bg={['#fd5750', '#fd5750', '#fd5750', '#fd5750', 'white']}
     {...props}
   >
-    sign up
+    subscribe
   </Button>
 )
 
@@ -66,7 +71,7 @@ const BlogPrefooter = () => (
                 height: 36,
                 bg: "#ffffff33",
                 fontSize: 1,
-                placeholder: "enter email address",
+                placeholder: "email address",
                 id: "newsletter-emailfield"
               }}
               btnComponent={SubmitButton}
@@ -80,41 +85,6 @@ const BlogPrefooter = () => (
           </Flex>
         </ResponsiveStack>
       </Background>
-    </Relative>
-
-    <Relative
-      m={15}
-      display={['block', 'block', 'none']}
-      top='-360px'
-      mb='-290px'
-    >
-      <Card border='1px solid #fd5750'>
-        <Background>
-          <ResponsiveStack
-            bg='#ffffffe6'
-            py={2}
-          >
-            <Flex
-              width={1}
-              px={2}
-              pb={2}
-            >
-              <Title component={Heading.h5} />
-            </Flex>
-            <NewsLetterForm
-              wrapper={Flex.column}
-              wrapperProps={{ alignItems: 'center', width: 1, px: 2 }}
-              emailFieldProps={{
-                bg: "#8c8c8c33",
-                placeholder: "enter email address",
-                placeholderColor: "gray.3",
-                color: "gray.3",
-                id: "newsletter-emailfield"
-              }}
-            />
-          </ResponsiveStack>
-        </Background>
-      </Card>
     </Relative>
   </React.Fragment>
 )
