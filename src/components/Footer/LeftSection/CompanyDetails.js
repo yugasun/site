fbecd5c6a7d51beb8feeab28dedddd25e68825c0
Pravin <pdaryani@gmail.com>
2019-01-09 12:25:35 +0100

    removed made with love and 2018 > 2019 in old footer

--- src/components/Footer/LeftSection/CompanyDetails.js
+++ src/components/Footer/LeftSection/CompanyDetails.js
@@ -7,16 +7,10 @@ import { termsOfService, privacyPolicy } from 'src/constants/urls'
 // Renders Company details
 const CompanyDetails = () => (
   <Box width={1}>
-    <Box width={[1, 1, 1 / 2]}>
-      <P fontSize={0} fontFamily={'Soleil'} lineHeight={3} opacity='0.5' m={0}>
-        Made with love in San Francisco + Atlanta, Austria, Germany, Pakistan,
-        Poland, Nebraska &amp; Thailand
-      </P>
-    </Box>
     <ResponsiveStack pt={4} alignItems={['left', 'left', 'center']}>
       <Box width={[1 / 2, 1 / 2, 1]}>
         <P m={0} fontSize={0}>
-          Serverless, Inc. © 2018
+          Serverless, Inc. © 2019
         </P>
       </Box>
 
