dcf6829f17244c9b96f8fc9893d5476870d41786
Pravin <pdaryani@gmail.com>
2019-01-16 12:14:16 +0100

    updated TitleWithIconNew to allow custom icon sizes according to heading components

--- src/fragments/TitleWithIconNew.js
+++ src/fragments/TitleWithIconNew.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import { TextWithIcon } from 'serverless-design-system'
 import redRectangleDots from 'src/assets/images/redRectangleDots.svg'
+import redRectangleDotsSmall from 'src/assets/images/redRectangleDotsSmall.svg'
 import { Heading } from 'src/fragments/DesignSystem'
 
 const TitleWithIconNew = ({ children, headingComponent, ...otherProps }) => {
   const FinalHeadingComponent = headingComponent ? headingComponent : Heading.h2
+  const finalIconImage = headingComponent === Heading.h4 ? redRectangleDotsSmall : redRectangleDots
+  const finalIconTop = headingComponent === Heading.h4 ? ["-12px", "-12px", 0, "-12px", "-13px"] : ["-12px", "-12px", 0, "-12px", "-10px"]
+  const finalIconLeft = headingComponent === Heading.h4 ? ["-10px", "-12px", "-14px", "-60px"] : ["-10px", "-12px", "-14px", "-20px"]
+
   return (
   <TextWithIcon
-    iconSrc={redRectangleDots}
+    iconSrc={finalIconImage}
     iconHeight={["30px", "30px", "30px", "38px"]}
     iconWidth={["60px", "60px", "60px", "88px"]}
-    iconTop={["-12px", "-12px", "-12px", "-12px", "-5px"]}
-    iconLeft={["-10px", "-12px", "-14px", "-18px"]}
+    iconTop={finalIconTop}
+    iconLeft={finalIconLeft}
     backgroundSize='contain'
     backgroundPosition='center'
     backgroundRepeat='no-repeat'
   >
     <FinalHeadingComponent
-        fontSize={['32px', '32px', '32px', '32px', '48px']}
-        fontFamily='Soleil'
-        letterSpacing={['0.5px']}
-        lineHeight={['32px', '32px', '32px', '32px', '62px']}
         {...otherProps}
     >
       { children }
