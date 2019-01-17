366c8fdf86272d2ce0a293ccbbca1587fb4202b9
Pravin <pdaryani@gmail.com>
2019-01-17 16:00:31 +0100

    use-cases prefooter margin-top updated

--- src/components/pages/learn/use-cases/List/index.js
+++ src/components/pages/learn/use-cases/List/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import {
-  Box,
-  ResponsiveStack,
-} from 'serverless-design-system'
+import { Box, ResponsiveStack } from 'serverless-design-system'
 
 import { AutoScrollList } from 'src/fragments'
 import TechBox from './TechBox'
@@ -20,15 +17,13 @@ import pythonImage from 'src/assets/images/python.png'
 import flaskImage from 'src/assets/images/flask.png'
 import graphqlImage from 'src/assets/images/graphql.png'
 
-const renderContents = (contents) => (
+const renderContents = contents => (
   <React.Fragment>
-    {
-      contents.map((content, index) => (
-        <P key={index} mt={[16, 16, 2, 3]}>
-          {content}
-        </P>
-      ))
-    }
+    {contents.map((content, index) => (
+      <P key={index} mt={[16, 16, 2, 3]}>
+        {content}
+      </P>
+    ))}
   </React.Fragment>
 )
 
@@ -38,33 +33,39 @@ const comparisonData = [
     image: LearnUsecase01,
     body: (
       <React.Fragment>
-        {
-          renderContents([
-            'Serverless websites and applications can be written and deployed without the front-work of setting up infrastructure. As such, it is possible to launch a fully-functional site in days. The best part is that serverless backends scale automatically with demand. No need to fear crashing when you get sudden surges of traffic.'
-          ])
-        }
+        {renderContents([
+          'Serverless websites and applications can be written and deployed without the front-work of setting up infrastructure. As such, it is possible to launch a fully-functional site in days. The best part is that serverless backends scale automatically with demand. No need to fear crashing when you get sudden surges of traffic.',
+        ])}
         <Box mt={[2]} mb={[5]}>
-          <Heading.h5
-          mb={[4]}
-          >
+          <Heading.h5 mb={[4]}>
             Want to get started? Check out these walkthroughs:
           </Heading.h5>
-          <ResponsiveStack
-            alignItems={[ "center", "center", "left" ]}>
-            <TechBox upperImage={nodeJSImage} lowerImage={expressImage} to='/blog/serverless-express-rest-api/'/>
-            <TechBox upperImage={pythonImage} lowerImage={flaskImage} to='/blog/flask-python-rest-api-serverless-lambda-dynamodb/'/>
-            <TechBox upperImage={graphqlImage} to='/blog/make-serverless-graphql-api-using-lambda-dynamodb/'/>
+          <ResponsiveStack alignItems={['center', 'center', 'left']}>
+            <TechBox
+              upperImage={nodeJSImage}
+              lowerImage={expressImage}
+              to='/blog/serverless-express-rest-api/'
+            />
+            <TechBox
+              upperImage={pythonImage}
+              lowerImage={flaskImage}
+              to='/blog/flask-python-rest-api-serverless-lambda-dynamodb/'
+            />
+            <TechBox
+              upperImage={graphqlImage}
+              to='/blog/make-serverless-graphql-api-using-lambda-dynamodb/'
+            />
           </ResponsiveStack>
         </Box>
       </React.Fragment>
-    )
+    ),
   },
   {
     title: 'Event streaming',
     image: LearnUsecase02,
     body: renderContents([
-      'Serverless compute can be triggered from pub/sub topics or from event logs, giving you elastic, scalable event pipelines without the maintenance of complicated clusters. These event streaming pipelines can power your analytics systems, update secondary data stores and caches, or feed monitoring systems.'
-    ])
+      'Serverless compute can be triggered from pub/sub topics or from event logs, giving you elastic, scalable event pipelines without the maintenance of complicated clusters. These event streaming pipelines can power your analytics systems, update secondary data stores and caches, or feed monitoring systems.',
+    ]),
   },
   {
     title: 'Image and Video Manipulation',
@@ -73,7 +74,7 @@ const comparisonData = [
       'Serverless allows you to build performance-enhancing image and video services for any application. You can use serverless services to do things like dynamically resize images or change video transcoding for different target devices.',
       'Applications are also increasingly relying on things like image recognition to improve user experience: for example, an ecommerce app that allows customers to photograph the front of their credit card instead of typing the numbers in manually.',
       'With Serverless, you can tap into the Google Vision API or Amazon Rekognition to recognize faces and images (e.g. for profile photos), or flag inappropriate content. You can automatically process and reformat user-uploaded images, or resize them into specific thumbnail dimensions.',
-    ])
+    ]),
   },
   {
     title: 'Processing Events and SaaS',
@@ -82,23 +83,23 @@ const comparisonData = [
       `Modern applications are incredibly modular, in that they heavily utilize software from 3rd party SaaS providers (GitHub, Twilio, Auth0, Stripe) to deliver functionality. These SaaS providers don’t let you run functions on their system, but they do emit events.`,
       'Serverless lets you subscribe functions to those events, regardless of where those functions are hosted. You can use this to take action.',
       'Pipe user data and activities to Salesforce to enhance lead flow. React to Stripe payments being processed. Automate GitHub workflows with functions running as webhook listeners. There are many, many rich possibilities here.',
-    ])
+    ]),
   },
   {
     title: 'Hybrid Cloud Applications',
     image: LearnUsecase05,
     body: renderContents([
       'Individual cloud providers often don’t meet every business requirement on their own. Teams choose to utilize the strongest features from each vendor, requiring them to deploy services to multiple providers inside a single application. But the cloud providers themselves do not make it easy to do this.',
-      'Our tooling abstracts away any necessary tweaking required to get functions to fit cloud vendors’ unique formats. This lets you easily deploy to any cloud provider you choose, thus maximizing the efficacy of your application and utilizing the best each cloud vendor has to offer.'
-    ])
+      'Our tooling abstracts away any necessary tweaking required to get functions to fit cloud vendors’ unique formats. This lets you easily deploy to any cloud provider you choose, thus maximizing the efficacy of your application and utilizing the best each cloud vendor has to offer.',
+    ]),
   },
   {
     title: 'Multi-language Applications',
     image: LearnUsecase06,
     body: renderContents([
       'When building an application, the first discussion is usually about which language to use. The language chosen isn’t always about what suits the project best, but rather which resources are already on-hand.',
-      'With serverless, applications can be multilingual. This fosters polyglot teams and enables seamless integration of services into an application that were written by specialized teams. It further prevents teams from getting locked into using the same language as their legacy software indefinitely; new languages can be used for new services and still talk to legacy services as needed.'
-    ])
+      'With serverless, applications can be multilingual. This fosters polyglot teams and enables seamless integration of services into an application that were written by specialized teams. It further prevents teams from getting locked into using the same language as their legacy software indefinitely; new languages can be used for new services and still talk to legacy services as needed.',
+    ]),
   },
   {
     title: 'Continuous Integration and Continuous Deployment (CI/CD)',
@@ -106,16 +107,13 @@ const comparisonData = [
     body: renderContents([
       'The ability to rapidly iterate software is more important than it has ever been. CI/CD pipelines allow you to ship code in small increments, so that bug fixes and other updates can be shipped on a daily basis.',
       'Serverless can automate many of these processes. Code checkins can trigger website builds and automatic redeploys, or PRs can trigger running automated tests to make sure code is well-tested before human review.',
-      'When you think about automation possibilities with Serverless Applications, it becomes easy to cut manual tasks out of your workflow.'
-    ])
+      'When you think about automation possibilities with Serverless Applications, it becomes easy to cut manual tasks out of your workflow.',
+    ]),
   },
 ]
 
 export default () => (
-  <Box
-    width={1}
-    pb={[12, 12, 6, 0]}
-  >
+  <Box width={1} pb={['420px', '420px', 14, 12]}>
     <AutoScrollList listData={comparisonData} />
   </Box>
 )
