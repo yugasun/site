b9ec3d8a63f08d7103b00b4a04f5f94d5232b21e
Pravin <pdaryani@gmail.com>
2019-01-23 14:50:25 +0100

    updated terminal text

--- src/components/pages/home/Hero/Image.js
+++ src/components/pages/home/Hero/Image.js
@@ -85,13 +85,13 @@ const HomeHeroImage = ({ usingSafariDesktop }) => (
         1 &nbsp;&nbsp;&nbsp;&nbsp;# Step 1. Install serverless globally
         <br />2 &nbsp;&nbsp;&nbsp;&nbsp;
         <Text.span color='#fff'>$ npm install serverless -g</Text.span>
-        <br />3<br />4 &nbsp;&nbsp;&nbsp;&nbsp;#Step 3. Create a serverless
+        <br />3<br />4 &nbsp;&nbsp;&nbsp;&nbsp;# Step 2. Create a serverless
         function
         <br />5 &nbsp;&nbsp;&nbsp;&nbsp;
         <Text.span color='#fff'>
           $ serverless create --template hello-world
         </Text.span>
-        <br />6 <br />7 &nbsp;&nbsp;&nbsp;&nbsp;# Step 4. deploy to cloud
+        <br />6 <br />7 &nbsp;&nbsp;&nbsp;&nbsp;# Step 3. deploy to cloud
         provider
         <br />8 &nbsp;&nbsp;&nbsp;&nbsp;
         <Text.span color='#fff'>$ serverless deploy</Text.span>
