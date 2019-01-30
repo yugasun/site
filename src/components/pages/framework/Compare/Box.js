b9479d2b47a032ceb3d14bb82b4d82a3e6f600ab
Pravin <pdaryani@gmail.com>
2019-01-30 15:53:00 +0100

    changed framework comparison boxes text

--- src/components/pages/framework/Compare/Box.js
+++ src/components/pages/framework/Compare/Box.js
@@ -3,44 +3,42 @@ import { Box, Text, HorizontalRule, Row, P } from 'serverless-design-system'
 import styled from 'styled-components'
 
 const Point = styled(P)`
-    font-size: 12px;
-    line-height: 16px;
-    letter-spacing: 0;
-    color: white;
-    margin-bottom: 17px;
-    font-family: 'Soleil';
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0;
+  color: white;
+  margin-bottom: 17px;
+  font-family: 'Soleil';
 `
 
 const HorizontalStyle = styled(HorizontalRule)`
-    border-top-style: solid;
-    border-color: #eaeaea;
+  border-top-style: solid;
+  border-color: #eaeaea;
 `
 
-const ComparisonBox = ({points, heading}) => (
-            <Box color='white' px={'32px'} pt={'92px'} pb={'16px'}>
-                <Text fontSize={['16px']} letterSpacing='0.67px' m={0}>
-                    serverless framework
-                </Text>
-                <Text fontSize={['36px']} letterSpacing={['1.5px']} mb={['62px']}>
-                    {heading}
-                </Text>
-                
-                {
-                points.map((point) => (
-                    <Fragment key={point}>
-                    <HorizontalStyle color='#eaeaea' height='1px' width='100%'/>
-                    <Row>
-                        <Box width='50%'>
-                            <Point>{point[0]}</Point>
-                        </Box>
-                        <Box width='50%'>
-                        <Point>{point[1]}</Point>
-                        </Box>
-                    </Row>
-                    </Fragment>
-                ))
-                }
-            </Box>
+const ComparisonBox = ({ points, heading }) => (
+  <Box color='white' px={'32px'} pt={'92px'} pb={'16px'}>
+    <Text fontSize={['16px']} letterSpacing='0.67px' m={0}>
+      serverless framework
+    </Text>
+    <Text fontSize={['36px']} letterSpacing={['1.5px']} mb={['62px']}>
+      {heading}
+    </Text>
+
+    {points.map(point => (
+      <Fragment key={point}>
+        <HorizontalStyle color='#eaeaea' height='1px' width='100%' />
+        <Row>
+          <Box width='50%' pr={[0, 0, 0, 0, 3]}>
+            <Point>{point[0]}</Point>
+          </Box>
+          <Box width='50%'>
+            <Point>{point[1]}</Point>
+          </Box>
+        </Row>
+      </Fragment>
+    ))}
+  </Box>
 )
 
 export default ComparisonBox
--- src/components/pages/framework/Compare/index.js
+++ src/components/pages/framework/Compare/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import CompareBox from './Box'
-import { Box, Background, Flex, Image, Absolute } from 'serverless-design-system'
-import { AppContainerNewest as AppContainer, WhiteButtonNew as WhiteButton } from 'src/components'
+import {
+  Box,
+  Background,
+  Flex,
+  Image,
+  Absolute,
+} from 'serverless-design-system'
+import {
+  AppContainerNewest as AppContainer,
+  WhiteButtonNew as WhiteButton,
+} from 'src/components'
 import styled from 'styled-components'
 import spinningWheel from 'src/assets/images/pages/framework/spinning-wheel.svg'
 import spinningWheelAnimation from 'src/assets/images/pages/framework/spinning-wheel-animation.svg'
@@ -10,90 +19,137 @@ import { enterprise, developersMenu } from 'src/constants/urls'
 import { InternalLink } from 'src/fragments'
 
 const openSourcePoints = [
-    ["Infrastructure as code", "Community support"],
-    ["Provider agnostic", "Community plugins"],
-    ["Simple Serverless development", "Community examples"]
+  ['An open-source CLI for building and deploying serverless applications', ''],
+  ['Support for AWS, Google Cloud Platform, Microsoft Azure, and more', ''],
+  ['Community provided plugins, examples and support', ''],
 ]
 
 const enterprisePoints = [
-    ["Everything in open source plus…"],
-    ["Architectural reviews", "Enterprise enablement and best practices"],
-    ["Custom Serverless workshop", "24/7 enterprise support"],
-    ["Custom plugins", "Custom use-case templates"]
+  ['Everything in open source plus…'],
+  [
+    'Serverless Secrets, to help you onboard new developers efficiently and securely',
+    'Enterprise enablement and best practices workshop, led by serverless experts',
+  ],
+  [
+    'Serverless Safeguards, to ensure operational and security best practices are followed',
+    'Custom plugin and use case template development',
+  ],
+  [
+    'Serverless Insights to help you monitor, debug and optimize your applications',
+    'Business hour or 24/7 support options',
+  ],
 ]
 
 const EnterpriseComparisonBox = styled(Background)`
-    margin-top: 325px;
+  margin-top: 325px;
 
-    @media screen and (max-width: 1024px) {
-        margin-top: 16px;
-    }
+  @media screen and (max-width: 1024px) {
+    margin-top: 16px;
+  }
 `
 
 const OpenSourceComparisonBox = styled(Background)`
-    @media screen and (min-width: 1024px) {
-        margin-top: 200px;
-    }
+  @media screen and (min-width: 1024px) {
+    margin-top: 200px;
+  }
 `
 
 export default class ComparisonBoxes extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-          usingSafariDesktop: false,
-        }
+  constructor() {
+    super()
+    this.state = {
+      usingSafariDesktop: false,
     }
-    
-    componentDidMount() {
-        const windowGlobal = typeof window !== 'undefined' && window
-        const navigatorGlobal = typeof navigator !== 'undefined' && navigator
-    
-        
-        if(windowGlobal && navigatorGlobal) {
-          const using_safari_browser = /^((?!chrome|android).)*safari/i.test(navigatorGlobal.userAgent)
-          const using_desktop = windowGlobal.innerWidth > 1200
-    
-          if(using_safari_browser && using_desktop) {
-            this.setState({usingSafariDesktop: true})
-          }
-        }
-    }
+  }
 
-    render() {
-        return (
-            <AppContainer>
-            <Heading.h3 align='center'>Easy, open Serverless development.</Heading.h3>
-            <Flex mb={['520px', '520px', '520px', '520px', '400px', '550px']} flexDirection={['column', 'column', 'column', 'column','row']} mt={[62, 62, 92, 92, 46]}>  
-            <Box display={[ 'none', 'none', 'none', 'none', 'block' ]}>
-                <Absolute right='0' zIndex='-1' width={[0, 0, 0, 0, '40%', '680px']}>
-                    <Image src={this.state.usingSafariDesktop ? spinningWheel : spinningWheelAnimation} />
-                </Absolute>
-            </Box>
-            <Box display={[ 'none', 'none', 'none', 'none', 'block' ]} mt={[0, 0, 0, 0, '30%']}>
-                <Absolute left='0' zIndex='-1' width={[0, 0, 0, 0, '550px', '650px']}>
-                    <Image src={this.state.usingSafariDesktop ? spinningWheel : spinningWheelAnimation}/>
-                </Absolute>
-            </Box>
+  componentDidMount() {
+    const windowGlobal = typeof window !== 'undefined' && window
+    const navigatorGlobal = typeof navigator !== 'undefined' && navigator
 
-                <OpenSourceComparisonBox background='#fd5750' width={['auto', 'auto', 'auto', 'auto', '50%']} maxHeight={['auto', 'auto', '467px']}>
-                    <CompareBox points={openSourcePoints} heading='open source'/>
-                    <MobileCenteredBox pb={'32px'} px={'32px'} pt={'14px'}>
-                        <InternalLink to={developersMenu.docs}>
-                            <WhiteButton>install free</WhiteButton>
-                        </InternalLink>
-                    </MobileCenteredBox>
-                </OpenSourceComparisonBox>
-                <EnterpriseComparisonBox background='black' width={['auto', 'auto', 'auto', 'auto', '50%']}>
-                    <CompareBox points={enterprisePoints} heading='enterprise'/>
-                    <MobileCenteredBox pb={'32px'} px={'32px'} pt={'14px'}>
-                        <InternalLink to={enterprise}>
-                            <Button m='auto' className='enterprise-learn-more'>learn more</Button>
-                        </InternalLink>
-                    </MobileCenteredBox>
-                </EnterpriseComparisonBox>
-            </Flex>
-        </AppContainer>
-        )
+    if (windowGlobal && navigatorGlobal) {
+      const using_safari_browser = /^((?!chrome|android).)*safari/i.test(
+        navigatorGlobal.userAgent
+      )
+      const using_desktop = windowGlobal.innerWidth > 1200
+
+      if (using_safari_browser && using_desktop) {
+        this.setState({ usingSafariDesktop: true })
+      }
     }
-    
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <Heading.h3 align='center'>
+          Easy, open Serverless development.
+        </Heading.h3>
+        <Flex
+          mb={['520px', '520px', '520px', '520px', '400px', '550px']}
+          flexDirection={['column', 'column', 'column', 'column', 'row']}
+          mt={[62, 62, 92, 92, 46]}
+        >
+          <Box display={['none', 'none', 'none', 'none', 'block']}>
+            <Absolute
+              right='0'
+              zIndex='-1'
+              width={[0, 0, 0, 0, '40%', '680px']}
+            >
+              <Image
+                src={
+                  this.state.usingSafariDesktop
+                    ? spinningWheel
+                    : spinningWheelAnimation
+                }
+              />
+            </Absolute>
+          </Box>
+          <Box
+            display={['none', 'none', 'none', 'none', 'block']}
+            mt={[0, 0, 0, 0, '30%']}
+          >
+            <Absolute
+              left='0'
+              zIndex='-1'
+              width={[0, 0, 0, 0, '550px', '650px']}
+            >
+              <Image
+                src={
+                  this.state.usingSafariDesktop
+                    ? spinningWheel
+                    : spinningWheelAnimation
+                }
+              />
+            </Absolute>
+          </Box>
+
+          <OpenSourceComparisonBox
+            background='#fd5750'
+            width={['auto', 'auto', 'auto', 'auto', '50%']}
+            maxHeight={['auto', 'auto', '515px']}
+          >
+            <CompareBox points={openSourcePoints} heading='open source' />
+            <MobileCenteredBox pb={'32px'} px={'32px'} pt={'14px'}>
+              <InternalLink to={developersMenu.docs}>
+                <WhiteButton>install free</WhiteButton>
+              </InternalLink>
+            </MobileCenteredBox>
+          </OpenSourceComparisonBox>
+          <EnterpriseComparisonBox
+            background='black'
+            width={['auto', 'auto', 'auto', 'auto', '50%']}
+          >
+            <CompareBox points={enterprisePoints} heading='enterprise' />
+            <MobileCenteredBox pb={'32px'} px={'32px'} pt={'14px'}>
+              <InternalLink to={enterprise}>
+                <Button m='auto' className='enterprise-learn-more'>
+                  learn more
+                </Button>
+              </InternalLink>
+            </MobileCenteredBox>
+          </EnterpriseComparisonBox>
+        </Flex>
+      </AppContainer>
+    )
+  }
 }
