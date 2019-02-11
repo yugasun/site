7da0d7662cfba9c64a00fc7092557e74418cf954
Pravin <pdaryani@gmail.com>
2019-02-11 09:08:54 +0100

    removed dynamically generated openpositions/data.js file

--- src/components/pages/company/jobs/OpenPositions/data.js
+++ src/components/pages/company/jobs/OpenPositions/data.js
@@ -1,19 +1,5 @@
 export default [
     {
-        "heading": "Growth",
-        "title": "Customer Success Engineer",
-        "location": "San Francisco/Remote",
-        "status": "Full-time",
-        "link": "https://jobs.lever.co/serverless/dfe8f81b-a526-4e5c-897a-3a662be8ba87"
-    },
-    {
-        "heading": "Growth",
-        "title": "Developer Advocate",
-        "location": "San Francisco/Remote",
-        "status": "Full-time",
-        "link": "https://jobs.lever.co/serverless/efa6e178-29eb-4f92-9d28-49fb393421cc"
-    },
-    {
         "heading": "Engineering",
         "title": "Senior Full Stack Engineer",
         "location": "San Francisco/Remote",
@@ -33,5 +19,12 @@ export default [
         "location": "San Francisco/Remote",
         "status": "Full-time",
         "link": "https://jobs.lever.co/serverless/43775278-aaca-4056-9a4a-90124141e30c"
+    },
+    {
+        "heading": "Growth",
+        "title": "Serverless Architect",
+        "location": "San Francisco/Remote",
+        "status": "Full-time",
+        "link": "https://jobs.lever.co/serverless/efbc8303-9ca2-4b3c-9b57-fea6149807aa"
     }
 ]
