cde5b35364beac95a393d4f5382772a450624921
Pravin <pdaryani@gmail.com>
2019-02-26 07:50:33 +0100

    enterprise page text animation removed (for now)

--- src/components/pages/enterprise/Hero/Benefit.js
+++ src/components/pages/enterprise/Hero/Benefit.js
@@ -2,21 +2,6 @@ import React from 'react'
 
 import { Box, Text } from 'serverless-design-system'
 import { Heading } from 'src/fragments/DesignSystem'
-import TextLoop from 'react-text-loop'
-import styled from 'styled-components'
-
-const TextLoopWithUnderline = styled(TextLoop)`
-  min-width: 165px;
-  text-align: left;
-  &:after {
-    content: '';
-    position: absolute;
-    left: 0;
-    display: inline-block;
-    width: 100%;
-    border-bottom: 1px solid;
-  }
-`
 
 const EnterpriseHeroBenefit = () => (
   <Box mt={[6, 6, 6, 6, 10]}>
@@ -29,12 +14,7 @@ const EnterpriseHeroBenefit = () => (
         letterSpacing={['-0.38px', '-0.38px', '-0.38px', '-0.38px', '-0.5px']}
         lineHeight={['38px', '38px', '38px', '38px', '44px']}
       >
-        We make{' '}
-        <TextLoopWithUnderline
-          interval={2500}
-          children={['cloud', 'Lambda', 'Azure', 'GCF', 'Kubernetes']}
-        />
-        &nbsp;simple
+        We make cloud simple
       </Text>
     </Box>
     <Box width={[1, 1, 0.69, 0.69, 0.49]} mx='auto' mt={35}>
