3823778848bd4613b571f54f5322331820810c5c
Pravin <pdaryani@gmail.com>
2019-03-19 19:30:05 +0100

    added components for /subscribe page

---
+++ src/components/pages/subscribe/Boxes/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Flex, Relative } from 'serverless-design-system'
+import { P } from 'src/fragments/DesignSystem'
+import { AppContainerNewest as AppContainer } from 'src/components'
+import LeftColumn from './LeftColumn'
+import RightColumn from './RightColumn'
+
+const SubscribeBoxes = () => (
+  <Relative height='470px' pt={['0', '0', '10vh', '10vh', '30vh']}>
+    <AppContainer>
+      <Flex
+        flexDirection={['column', 'column', 'column', 'column', 'row']}
+        alignItems={['center']}
+      >
+        <LeftColumn />
+        <RightColumn />
+      </Flex>
+      <P color='#5b5b5b' ml={[0, 0, 70, 90, 0]} mt={[4, 4, 2]} mb={[8, 8, 0]}>
+        Don’t worry. You can unsubscribe whenever you want.
+      </P>
+    </AppContainer>
+  </Relative>
+)
+
+export default SubscribeBoxes
---
+++ src/components/pages/subscribe/SpinningWheels.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Background, Absolute } from 'serverless-design-system'
+import spinningWheel from 'src/assets/images/pages/framework/spinning-wheel.svg'
+import spinningWheelAnimation from 'src/assets/images/pages/framework/spinning-wheel-animation.svg'
+
+class SpinningWheels extends Component {
+  constructor() {
+    super()
+    this.state = {
+      usingSafariDesktop: false,
+    }
+  }
+
+  componentDidMount() {
+    const windowGlobal = typeof window !== 'undefined' && window
+    const navigatorGlobal = typeof navigator !== 'undefined' && navigator
+
+    if (windowGlobal && navigatorGlobal) {
+      const using_safari_browser = /^((?!chrome|android).)*safari/i.test(
+        navigatorGlobal.userAgent
+      )
+      const using_desktop = windowGlobal.innerWidth > 1200
+
+      if (using_safari_browser && using_desktop) {
+        this.setState({ usingSafariDesktop: true })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Absolute
+          right='0'
+          bottom='0'
+          display={['none', 'none', 'none', 'none', 'block']}
+        >
+          <Background
+            width='622px'
+            height='553px'
+            backgroundImage={
+              this.state.usingSafariDesktop
+                ? `url(${spinningWheel})`
+                : `url(${spinningWheelAnimation})`
+            }
+            backgroundRepeat='no-repeat'
+          />
+        </Absolute>
+        <Absolute
+          top='0'
+          left='0'
+          zIndex='0'
+          width={[0, 0, 0, 0, '40%', '622px']}
+          display={['none', 'none', 'none', 'none', 'block']}
+        >
+          <Background
+            width='622px'
+            height='553px'
+            backgroundImage={
+              this.state.usingSafariDesktop
+                ? `url(${spinningWheel})`
+                : `url(${spinningWheelAnimation})`
+            }
+            backgroundRepeat='no-repeat'
+          />
+        </Absolute>
+      </React.Fragment>
+    )
+  }
+}
+
+export default SpinningWheels
