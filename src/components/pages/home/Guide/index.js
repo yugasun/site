f8d0eaea37676055a408a7e40b8436ab8eb3df30
Pravin <pdaryani@gmail.com>
2019-05-09 14:49:24 +0200

    Fix #3

--- src/components/pages/home/Guide/index.js
+++ src/components/pages/home/Guide/index.js
@@ -12,6 +12,7 @@ import guidesData from './Data'
 import { InternalLink } from 'src/fragments'
 import { P, Button } from 'src/fragments/DesignSystem'
 import styled from 'styled-components'
+require('./guides.css')
 
 const FlexWithLaptopWidth = styled(Flex)`
   @media screen and (min-width: 1200px) and (max-width: 1500px) {
@@ -21,6 +22,20 @@ const FlexWithLaptopWidth = styled(Flex)`
 const BackgroundWithBoxShadow = styled(Background)`
   box-shadow: 2px 2px 8px 0 rgba(0, 0, 0, 0.08);
   border: solid 1px rgba(234, 234, 234, 0.3);
+
+  @media screen and (min-width: 1200px) and (max-width: 1350px) {
+    min-height: 588px;
+  }
+`
+
+const BackgroundWithCustomHeight = styled(Background)`
+  @media screen and (min-width: 1200px) and (max-width: 1350px) {
+    height: 700px;
+  }
+
+  @media screen and (min-width: 1351px) and (max-width: 1390px) {
+    height: 700px;
+  }
 `
 
 const TitleText = ({ children, color }) => (
@@ -37,7 +52,10 @@ const TitleText = ({ children, color }) => (
 )
 
 const DevelopersGuide = () => (
-  <Background background='#f7f7f7' height={[2030, 2030, 2575, 2575, 604]}>
+  <BackgroundWithCustomHeight
+    background='#f7f7f7'
+    height={[2030, 2030, 2575, 2575, 604]}
+  >
     <AppContainer>
       <Flex
         flexDirection={['column', 'column', 'column', 'column', 'row']}
@@ -79,41 +97,47 @@ const DevelopersGuide = () => (
               <Box py={[42, 42, 52]} px={[26, 26, 118, 118, 32]}>
                 <TitleText color={'#fd5750'}>{guide.metaTitle}</TitleText>
                 <TitleText color={'#000000'}>{guide.title}</TitleText>
-                <P align={['center']}>{guide.content}</P>
-                <Flex.verticallyCenter
-                  justifyContent='space-between'
-                  flexDirection={[
-                    'column',
-                    'column',
-                    'row',
-                    'row',
-                    'column',
-                    'row',
-                  ]}
-                  pt={22}
-                >
-                  <InternalLink to={guide.actions[0].link}>
-                    <Button>{guide.actions[0].text}</Button>
-                  </InternalLink>
-                  <InternalLink to={guide.actions[1].link}>
-                    <Text
-                      fontSize='16px'
-                      lineHeight='14px'
-                      letterSpacing='0.67px'
-                      color='#fd5750'
-                      mt={[32, 32, 0, 0, 32, 0]}
-                    >
-                      {guide.actions[1].text}
-                    </Text>
-                  </InternalLink>
-                </Flex.verticallyCenter>
+                <Flex flexDirection='column'>
+                  <P align={['center']}>{guide.content}</P>
+                  <Flex.verticallyCenter
+                    justifyContent='space-between'
+                    flexDirection={[
+                      'column',
+                      'column',
+                      'row',
+                      'row',
+                      'column',
+                      'row',
+                    ]}
+                    pt={22}
+                    className={`homepage-guides-${guide.actions[0].text.replace(
+                      ' ',
+                      '-'
+                    )}`}
+                  >
+                    <InternalLink to={guide.actions[0].link}>
+                      <Button>{guide.actions[0].text}</Button>
+                    </InternalLink>
+                    <InternalLink to={guide.actions[1].link}>
+                      <Text
+                        fontSize='16px'
+                        lineHeight='14px'
+                        letterSpacing='0.67px'
+                        color='#fd5750'
+                        mt={[32, 32, 0, 0, 32, 0]}
+                      >
+                        {guide.actions[1].text}
+                      </Text>
+                    </InternalLink>
+                  </Flex.verticallyCenter>
+                </Flex>
               </Box>
             </BackgroundWithBoxShadow>
           </FlexWithLaptopWidth>
         ))}
       </Flex>
     </AppContainer>
-  </Background>
+  </BackgroundWithCustomHeight>
 )
 
 export default DevelopersGuide
