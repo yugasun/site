f2dad9d4af9bd1696cbc8767eb2946d1506fbcfe
Pravin <pdaryani@gmail.com>
2019-05-14 10:44:50 +0200

    enterprise support CTA -> /acceleration/

--- src/components/Header/EnterpriseSupportButton.js
+++ src/components/Header/EnterpriseSupportButton.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import NavLink from 'src/components/NavLink'
 import { SecondaryButton } from 'src/fragments/DesignSystem'
-import { enterprise } from 'src/constants/urls'
+import { developersMenu } from 'src/constants/urls'
 
 const EnterpriseSupportButton = () => (
-  <NavLink to={enterprise} completed className='header-menu-item'>
-      <SecondaryButton px={[0, 0, 0, 0, '0.3rem', '0.5rem']} width={['187px', '187px', '187px', '187px', '187px', '207px']}>
+  <NavLink
+    to={developersMenu.acceleration}
+    completed
+    className='header-menu-item'
+  >
+    <SecondaryButton
+      px={[0, 0, 0, 0, '0.3rem', '0.5rem']}
+      width={['187px', '187px', '187px', '187px', '187px', '207px']}
+    >
       enterprise support
-      </SecondaryButton>
-    </NavLink>
+    </SecondaryButton>
+  </NavLink>
 )
 
 export default EnterpriseSupportButton
