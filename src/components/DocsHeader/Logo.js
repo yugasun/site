e7d9b6639c5671c8de92f3f19e72033377784829
Pravin <pdaryani@gmail.com>
2019-05-22 12:51:19 +0200

    updated Docs and Blog header logo

--- src/components/DocsHeader/Logo.js
+++ src/components/DocsHeader/Logo.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import { Logo } from 'serverless-design-system'
-import logo from 'src/assets/images/docs-logo.svg'
+import { Logo, Row } from 'serverless-design-system'
+import logo from 'src/assets/images/logo.svg'
+import docsLogo from 'src/assets/images/docs-logo.svg'
 import styled from 'styled-components'
 
 const LogoWithDesktopTopMargin = styled(Logo)`
-  @media screen and (min-width: 1200px) {
-    margin-top: 6px;
-  }
+  margin-top: 6px;
+`
+
+const LogoWithDesktopLeftMargin = styled(Logo)`
+  margin-left: 8px;
 `
 
 const LogoComponent = () => (
-  <Link to='/'>
-    <LogoWithDesktopTopMargin
-      src={logo}
-      height={['21px', '21px', '21px', '55px']}
-      width={['124px', '124px', '124px', '209px']}
-      alt='Serverless'
-    />
-  </Link>
+  <Row alignItems='center'>
+    <Link to='/'>
+      <LogoWithDesktopTopMargin
+        src={logo}
+        height={['21px', '21px', '21px', '55px']}
+        width={['112px', '112px', '112px', '153px']}
+        alt='Serverless'
+      />
+    </Link>
+    <Link to='/framework/docs/'>
+      <LogoWithDesktopLeftMargin
+        src={docsLogo}
+        height={['15px', '15px', '15px', '17px']}
+        width={['36px', '36px', '36px', '45px']}
+        alt='Serverless'
+        ml={'8px'}
+      />
+    </Link>
+  </Row>
 )
 
 export default LogoComponent
