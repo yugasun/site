af999d16cf219d3f000112b19bf0fde3ebab94af
Pravin <pdaryani@gmail.com>
2019-07-14 20:34:12 +0200

    added current footer to home

---
+++ src/components/pages/home/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Prefooter from './Prefooter'
+import { Footer } from 'src/components'
+
+const index = props => (
+  <React.Fragment>
+    <Prefooter />
+    <Footer />
+  </React.Fragment>
+)
+
+export default index
--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Box, Column } from 'serverless-design-system'
 import { Header } from 'src/components'
-import Footer from 'src/components/pages/home/Footer/Prefooter'
+import Footer from 'src/components/pages/home/Footer'
 
 const DefaultLayout = ({ children, transparentHeader }) => {
   return (
