c80280d49d2820ac410ad9f6125b5adbec8e3975
Pravin <pdaryani@gmail.com>
2019-07-14 20:21:35 +0200

    deleted [FooterNew] component

--- src/layouts/Blog.js
+++ src/layouts/Blog.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { withTheme } from 'styled-components'
 import { Box, Column } from 'serverless-design-system'
-import { BlogHeader as Header, FooterNew as Footer } from 'src/components'
+import { BlogHeader as Header, FooterNewest as Footer } from 'src/components'
 
-const BlogLayout = ({ children, prefooter, transparentHeader, prefooterOnlyDesktop }) => {
-  const pbMobile = prefooterOnlyDesktop ? 62: 300
-  const prefooterOnlyDesktopBool = prefooterOnlyDesktop ? true: false
-  
+const BlogLayout = ({
+  children,
+  prefooter,
+  transparentHeader,
+  prefooterOnlyDesktop,
+}) => {
+  const pbMobile = prefooterOnlyDesktop ? 62 : 300
+  const prefooterOnlyDesktopBool = prefooterOnlyDesktop ? true : false
+
   return (
-      <Column width={1}>
-        <Header transparent={transparentHeader} />
-        <Box
-          width={1}
-          pb={[pbMobile, pbMobile, pbMobile, pbMobile, 250]}
-        >
-          {children}
-        </Box>
-        <Footer prefooter={prefooter} prefooterOnlyDesktop={prefooterOnlyDesktopBool} />
-      </Column>
-    )
-  
-  }
+    <Column width={1}>
+      <Header transparent={transparentHeader} />
+      <Box width={1} pb={[pbMobile, pbMobile, pbMobile, pbMobile, 250]}>
+        {children}
+      </Box>
+      <Footer
+        prefooter={prefooter}
+        prefooterOnlyDesktop={prefooterOnlyDesktopBool}
+      />
+    </Column>
+  )
+}
 
 export default withTheme(BlogLayout)
