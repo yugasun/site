c220c36ae27cbb8df500da98366486ff257fd02e
Pravin <pdaryani@gmail.com>
2019-07-15 19:58:29 +0200

    updated few video URLs

--- src/components/pages/home/Hero/FeaturesData.js
+++ src/components/pages/home/Hero/FeaturesData.js
@@ -2,11 +2,12 @@ export default [
   {
     name: 'Intro',
     videoUrl:
-      'https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/home/homepage-video.mp4',
+      'https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/home/SF_01Intro.mp4',
   },
   {
     name: 'Develop',
-    videoUrl: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+    videoUrl:
+      'https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/home/SF_02Develop.mp4',
   },
   {
     name: 'Deploy',
