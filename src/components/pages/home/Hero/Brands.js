2e79968d01e6f8062d4a82c86ec5a84bd5e7bda3
Pravin <pdaryani@gmail.com>
2019-07-16 14:36:56 +0200

    adjusted desktop margin-bottom from video

--- src/components/pages/home/Hero/Brands.js
+++ src/components/pages/home/Hero/Brands.js
@@ -11,7 +11,7 @@ import { TitleWithIconNewest as TitleWithIcon } from 'src/fragments'
 const HomeHeroFooter = () => (
   <AppContainer>
     <Flex
-      mt={[0, 0, 132, 132, 162]}
+      mt={[0, 0, 132, 132, 112, 132]}
       flexDirection={[
         'column-reverse',
         'column-reverse',
