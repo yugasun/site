1927417e7a3dd6edc5e0adc257a83bec8e1f2196
Pravin <pdaryani@gmail.com>
2019-07-29 20:12:38 +0200

    tabtop dropdown menu onHover fix

--- src/components/NavLink/index.js
+++ src/components/NavLink/index.js
@@ -9,7 +9,7 @@ const getLinkStyle = ({ backgroundOnHover }) => `
   position: relative;
   text-decoration: none;
 
-  @media screen and (max-width: 1024px) {
+  @media screen and (max-width: 1280px) {
     ${
       backgroundOnHover
         ? `
