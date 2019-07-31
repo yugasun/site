1891a0648119d895043e28b8354bdf9b5ef1cde0
Pravin <pdaryani@gmail.com>
2019-07-31 14:28:30 +0200

    updated removeUTM script
    from
    https://serverless.com/blog/notifications-and-alerts/?utm_source=social&utm_medium=twitter&utm_campaign=notifications-and-alerts
    
    before
    https://serverless.com/blog/notifications-and-alerts/-and-alerts
    
    after
    https://serverless.com/blog/notifications-and-alerts/

--- src/utils/analytics/source/removeUTM.js
+++ src/utils/analytics/source/removeUTM.js
@@ -5,8 +5,17 @@ const removeUTM = () => {
   if (typeof window === 'undefined') {
     return false
   }
-  if (window.location.search.indexOf('utm_') !== -1 && window.history && history.replaceState) {
-    let url = window.location.toString().replace(/(\&|\?)utm([_a-zA-Z0-9=\.%]+)/g, '')
+  if (
+    window.location.search.indexOf('utm_') !== -1 &&
+    window.history &&
+    history.replaceState
+  ) {
+    let url = window.location
+      .toString()
+      .replace(
+        /(\?)utm[^&]*(?:&utm[^&]*)*&(?=(?!utm[^\s&=]*=)[^\s&=]+=)|\?utm[^&]*(?:&utm[^&]*)*$|&utm[^&]*/gi,
+        '$1'
+      )
     if (url.indexOf('&') > -1 && url.indexOf('?') === -1) {
       url = url.replace(/&/, '?')
     }
