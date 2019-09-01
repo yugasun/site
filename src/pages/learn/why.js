db056e462691c2bf2cfb78150f27451d5570cb51
Pravin <pdaryani@gmail.com>
2019-09-01 18:31:57 +0200

    removed explanation component from learn page

--- src/pages/learn/why.js
+++ src/pages/learn/why.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import DefaultLayout from 'src/layouts/DefaultNewest'
 import Hero from 'src/components/pages/learn/overview/Hero'
-import Explanation from 'src/components/pages/learn/overview/Explanation'
 import ServerlessAppBenefits from 'src/components/pages/learn/overview/AppBenefits'
 import ServerlessFrameworkBenefits from 'src/components/pages/learn/overview/FrameworkBenefits'
 import AdditionalFeatures from 'src/components/pages/learn/overview/AdditionalFeatures'
@@ -17,7 +16,6 @@ const Overview = ({ location }) => (
       location={location}
     />
     <Hero />
-    <Explanation />
     <ServerlessAppBenefits />
     <ServerlessFrameworkBenefits />
     <AdditionalFeatures />
