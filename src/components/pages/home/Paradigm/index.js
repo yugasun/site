2607eb6367aab3673b477706236fe1aed0824ef5
Austen Collins <austen@serverless.com>
2019-09-02 11:10:25 -0700

    update mission

--- src/components/pages/home/Paradigm/index.js
+++ src/components/pages/home/Paradigm/index.js
@@ -118,7 +118,6 @@ export default class Paradigm extends React.Component {
               <p>Software is eating the world...  Unfortunately, most businesses still can't deliver software successfully, much less do so at the pace needed to stay competitive.  For those who wish to keep up, let alone lead, <b>software delivery & operation must be radically simplified.</b></p>
               <p>This is what the <b>Serverless Architecture</b> offers — It's built on next-generation public cloud services that auto-scale and charge only when used.  When scale, capacity planning & cost management are automated, the result is software that's easier to build, maintain, and often up to 99% cheaper.</p>
               <p>Serverless Architectures are new and therefore require a shift in how we previously thought about architectures & workflows. Our goal at <b>Serverless Inc.</b> is to give developers, teams and orgs all of the tools they need to build and operate serverless applications, in one simple, powerful & elegant experience... <b>The Serverless Framework</b>.</p>
-              <p>Together, we'll enable everyone to build more & manage less.</p>
             </div>
           </div>
         </div>
