6ef738b2b1b0ac586311d006df624cfe6e3a1cec
Pravin <pdaryani@gmail.com>
2019-09-10 13:45:57 +0200

    plugins searchField font == 'SoleilBk'

--- src/components/pages/plugins/Search/Field.js
+++ src/components/pages/plugins/Search/Field.js
@@ -59,7 +59,7 @@ export default class SearchField extends React.Component {
             width='100%'
             border='none'
             fontSize={14}
-            fontFamily='Serverless'
+            fontFamily='SoleilBk'
             placeholderColor='#8c8c8c'
             px={[1, 1, 2]}
             onChange={this.handQueryFilter}
