e396ccad5df04a0239514dea9301ca31abd6ce0a
Pravin <pdaryani@gmail.com>
2019-09-11 06:53:37 +0200

    added extend component to /integrations

---
+++ src/components/pages/integrations/Extend/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Flex } from 'serverless-design-system'
+import { Heading, P } from 'src/fragments/DesignSystem'
+
+const ExtendServerless = props => (
+  <Flex>
+    <Heading.h3>Extend Serverless with Plugins and Components</Heading.h3>
+    <P />
+  </Flex>
+)
+
+export default ExtendServerless
