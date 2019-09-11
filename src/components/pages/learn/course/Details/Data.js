b2651f7872485424a3daa2f6c53b9a32f31e880f
Pravin <pdaryani@gmail.com>
2019-09-11 06:56:48 +0200

    updated course playTime

--- src/components/pages/learn/course/Details/Data.js
+++ src/components/pages/learn/course/Details/Data.js
@@ -4,7 +4,7 @@ module.exports = [
     items: [
       {
         title: 'Purpose of the course',
-        playTime: '5:50',
+        playTime: '2:11',
         videoNumber: 1,
         videoLink: 'https://www.youtube.com/watch?v=XTJImzRH8aY',
       },
@@ -15,48 +15,48 @@ module.exports = [
     items: [
       {
         title: 'What is Serverless Development?',
-        playTime: '5:50',
+        playTime: '6:03',
         videoNumber: 2,
         videoLink: 'https://www.youtube.com/watch?v=ki5V1TGR75U',
       },
       {
         title: 'Getting setup with AWS',
-        playTime: '5:50',
+        playTime: '2:24',
         videoNumber: 3,
         videoLink: 'https://www.youtube.com/watch?v=3rcwUa_H9Ok',
       },
       {
         title: 'Build Serverless Endpoint Manually',
-        playTime: '5:50',
+        playTime: '7:14',
         videoNumber: 4,
         videoLink: 'https://www.youtube.com/watch?v=0_NcB2Sw15k',
       },
       {
         title: 'Installing the Serverless Framework',
-        playTime: '5:50',
+        playTime: '2:22',
         videoNumber: 5,
         videoLink: 'https://www.youtube.com/watch?v=NjZaXwNU08Q',
       },
       {
         title: 'Build Serverless Endpoint using Serverless Framework',
-        playTime: '5:50',
+        playTime: '5:33',
         videoNumber: 6,
         videoLink: 'https://www.youtube.com/watch?v=nwG9fIRnIDY',
       },
       {
         title: 'Deploying via Dashboard profile',
-        playTime: '5:50',
+        playTime: '6:05',
         videoNumber: 7,
         videoLink: 'https://www.youtube.com/watch?v=KTsWDCXvxqU',
       },
       {
         title: '[coming soon] What happens when we deploy',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 8,
       },
       {
         title: '[coming soon] Removing our service',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 9,
       },
     ],
@@ -66,37 +66,37 @@ module.exports = [
     items: [
       {
         title: '[coming soon] What is Cloud Computing?',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 10,
       },
       {
         title: '[coming soon] AWS Lambda',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 11,
       },
       {
         title: '[coming soon] API Gateway',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 12,
       },
       {
         title: '[coming soon] DynamoDB Basics',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 13,
       },
       {
         title: '[coming soon] SNS',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 14,
       },
       {
         title: '[coming soon] SQS',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 15,
       },
       {
         title: '[coming soon] S3',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 16,
       },
     ],
@@ -106,27 +106,27 @@ module.exports = [
     items: [
       {
         title: '[coming soon] Introduction to our first service',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 17,
       },
       {
         title: '[coming soon] Local Development',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 18,
       },
       {
         title: '[coming soon] Authentication/Authorisation',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 19,
       },
       {
         title: '[coming soon] User Creation',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 20,
       },
       {
         title: '[coming soon] Deploy and Integration test',
-        playTime: '5:50',
+        playTime: '0:00',
         videoNumber: 21,
       },
     ],
