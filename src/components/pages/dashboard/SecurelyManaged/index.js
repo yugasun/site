00e0f47a362662320b6698b667c06a929e71a8eb
Pravin <pdaryani@gmail.com>
2019-09-16 16:44:54 +0200

    added images for securelyManaged component

--- src/components/pages/dashboard/SecurelyManaged/index.js
+++ src/components/pages/dashboard/SecurelyManaged/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Flex } from 'serverless-design-system'
+import { Flex, Image, Box } from 'serverless-design-system'
 import securelyManage1Image from 'src/assets/images/pages/dashboard/securely-manage-1.png'
+import securelyManage2Image from 'src/assets/images/pages/dashboard/securely-manage-2.png'
 import Content from './Content'
-import { ImageWithShadow as Image } from 'src/fragments'
 
 const CustomPolicies = props => (
   <Flex
@@ -11,11 +11,20 @@ const CustomPolicies = props => (
     flexDirection={['column', 'column', 'row']}
   >
     <Content />
-    <Image
-      src={securelyManage1Image}
-      width={['100%', '100%', 336, 450, 520, 592]}
-      height={[110, 110, 123, 170, 200, 217]}
-    />
+    <Flex flexDirection='column'>
+      <Image
+        src={securelyManage1Image}
+        width={['100%', '100%', 350, 450, 463, 463]}
+        height={[226, 226, 226, 336, 336, 336]}
+      />
+      <Box ml={[75, 75, 100, 131]} mt={[-52, -52, -52, -72]}>
+        <Image
+          src={securelyManage2Image}
+          width={['100%', '100%', 326, 426, 435, 435]}
+          height={[300, 300, 326, 426, 426, 426]}
+        />
+      </Box>
+    </Flex>
   </Flex>
 )
 
