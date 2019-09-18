a12a1b0f0e65839b9ffff1ae16faf6b5afa944d6
Pravin <pdaryani@gmail.com>
2019-09-18 15:31:34 +0200

    updated /enterprise page to match other form pages

---
+++ src/components/pages/enterprise/Content/Data.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    title: 'Integrations',
+    description:
+      'Send alerts, metrics and more to Slack, Email, AWS SNS, Webhooks and other Ops tools.',
+  },
+  {
+    title: 'Policies',
+    description:
+      'Set and automate policy enforcement of standards and best practices across teams.',
+  },
+]
