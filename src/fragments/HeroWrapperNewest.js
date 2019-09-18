dd54ca0640608b8adf3fd762dcda6e53166c6632
Pravin <pdaryani@gmail.com>
2019-09-18 10:46:14 +0200

    added standard bottom padding for hero (form of background)

--- src/fragments/HeroWrapperNewest.js
+++ src/fragments/HeroWrapperNewest.js
@@ -5,15 +5,18 @@ import { Background, Box } from 'serverless-design-system'
 import { AppContainer } from 'src/components'
 
 const HeroWrapper = ({ children, theme, ...otherProps }) => (
-  <Background {...otherProps}>
-    <Box width={1} pt={[92, 92, 72, 72, 115, 85]}>
-      <AppContainer>
-        <Box pb={[62, 62, 62, 62, 92]} pt={['14px', '14px', 4, 4, '15px']}>
-          {children}
-        </Box>
-      </AppContainer>
-    </Box>
-  </Background>
+  <React.Fragment>
+    <Background {...otherProps}>
+      <Box width={1} pt={[92, 92, 72, 72, 115, 85]}>
+        <AppContainer>
+          <Box pb={[62, 62, 62, 62, 92]} pt={['14px', '14px', 4, 4, '15px']}>
+            {children}
+          </Box>
+        </AppContainer>
+      </Box>
+    </Background>
+    <Background background='#f7f7f7' height={[62, 62, 62, 62, 92]} />
+  </React.Fragment>
 )
 
 export default withTheme(HeroWrapper)
