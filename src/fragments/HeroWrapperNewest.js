a3a8912af583be449ce9b71a5b92202e61b911bc
Pravin <pdaryani@gmail.com>
2019-09-18 10:22:54 +0200

    updated policies hero to match mock

--- src/fragments/HeroWrapperNewest.js
+++ src/fragments/HeroWrapperNewest.js
@@ -6,16 +6,7 @@ import { AppContainer } from 'src/components'
 
 const HeroWrapper = ({ children, theme, ...otherProps }) => (
   <Background {...otherProps}>
-    <Box
-      width={1}
-      pt={[
-        theme.navbarHeights.mobile,
-        theme.navbarHeights.mobile,
-        '80px',
-        '80px',
-        '85px',
-      ]}
-    >
+    <Box width={1} pt={[92, 92, 72, 72, 115, 85]}>
       <AppContainer>
         <Box pb={[62, 62, 62, 62, 92]} pt={['14px', '14px', 4, 4, '15px']}>
           {children}
