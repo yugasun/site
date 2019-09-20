b6b5937e0ab7abecc4013a575378d2cf928c3575
Pravin <pdaryani@gmail.com>
2019-09-20 19:41:22 +0200

    updated debuggingHeroImage

--- src/components/pages/debugging/Hero/Image.js
+++ src/components/pages/debugging/Hero/Image.js
@@ -7,8 +7,8 @@ const HeroImage = props => (
   <Box mt={[0, 0, 0, 0, '-20px', 0]}>
     <Image
       src={debuggingHeroImage}
-      width={['100%', '100%', 592, 592, 512, 592]}
-      height={[250, 250, 347, 347, 347, 347]}
+      width={['100%', '100%', 592, 592, 510, 592]}
+      height={[250, 250, 347, 347, 305, 347]}
     />
   </Box>
 )
