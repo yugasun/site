a83b9f0a0d02e49a25bfc2310a2e2efebf2a8fda
Pravin <pdaryani@gmail.com>
2019-09-22 15:56:05 +0200

    added link to /learn

--- src/components/pages/home/UserTweets/index.js
+++ src/components/pages/home/UserTweets/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Box, Row } from 'serverless-design-system'
 import imgTwitter from 'src/assets/images/home/tweeters/twitter-icon.svg'
 import imgTweeterOne from 'src/assets/images/home/tweeters/papa.png'
 import imgTweeterTwo from 'src/assets/images/home/tweeters/david.png'
 import imgTweeterThree from 'src/assets/images/home/tweeters/jason.png'
 import imgTweeterFour from 'src/assets/images/home/tweeters/tyler.png'
 import './UserTweets.css'
+import { learn } from 'src/constants/urls'
+import { InternalLink } from 'src/fragments'
 
 export default class UserTweets extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {}
@@ -28,10 +28,7 @@ export default class UserTweets extends React.Component {
     return (
       <div className='container user-tweets-component'>
         <div className='container-inner user-tweets-inner'>
-
-          <div className='container header-label'>
-            Serverless Architects
-          </div>
+          <div className='container header-label'>Serverless Architects</div>
 
           <div className='container header'>
             Empowering a new generation of developers.
@@ -47,7 +44,10 @@ export default class UserTweets extends React.Component {
                 @david_welch
               </div>
               <div className='user-tweets-tweet-text'>
-                Built our platform on @goserverless with 2 engineers working nights and mornings for the first 14 months. Didn’t pay a dime until 16 months in, have scaled to 10+ employees w exp from 0 to senior, very agile w CI/CD, couldn’t have made a better choice.
+                Built our platform on @goserverless with 2 engineers working
+                nights and mornings for the first 14 months. Didn’t pay a dime
+                until 16 months in, have scaled to 10+ employees w exp from 0 to
+                senior, very agile w CI/CD, couldn’t have made a better choice.
               </div>
             </div>
 
@@ -60,7 +60,8 @@ export default class UserTweets extends React.Component {
                 @DrPappa
               </div>
               <div className='user-tweets-tweet-text'>
-                I didn't realise #serverless was so absurdly quick and easy to use. Thanks @goserverless you've done an excellent job.
+                I didn't realise #serverless was so absurdly quick and easy to
+                use. Thanks @goserverless you've done an excellent job.
               </div>
             </div>
 
@@ -73,7 +74,10 @@ export default class UserTweets extends React.Component {
                 @themccallister
               </div>
               <div className='user-tweets-tweet-text'>
-                I’m still a huge fan of @Docker but man this @goserverless stuff is purely amazing, I'm fully onboard with managing infrastructure from code... Just moved an existing API and supporting app to Lambda functions in one day...
+                I’m still a huge fan of @Docker but man this @goserverless stuff
+                is purely amazing, I'm fully onboard with managing
+                infrastructure from code... Just moved an existing API and
+                supporting app to Lambda functions in one day...
               </div>
             </div>
 
@@ -86,15 +90,21 @@ export default class UserTweets extends React.Component {
                 @TylerZey
               </div>
               <div className='user-tweets-tweet-text'>
-                Built two #Lambda functions today via @goserverless. One runs daily to update all of our coupons in DynamoDB. And the other is the query function to search DynamoDB for the coupon. Super duper easy with Serverless framework. Got it live and almost in prod already.
+                Built two #Lambda functions today via @goserverless. One runs
+                daily to update all of our coupons in DynamoDB. And the other is
+                the query function to search DynamoDB for the coupon. Super
+                duper easy with Serverless framework. Got it live and almost in
+                prod already.
               </div>
             </div>
           </div>
 
           <div className='user-tweets-actions container'>
-            Serverless Architects are a new and fast growing role - <a>Learn how to build Serverless Applications here</a>
+            Serverless Architects are a new and fast growing role -{' '}
+            <InternalLink to={learn}>
+              <span>Learn how to build Serverless Applications here</span>
+            </InternalLink>
           </div>
-
         </div>
       </div>
     )
