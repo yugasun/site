afcd47db297ff8d55ac6341f570926de0eca3fcb
Pravin <pdaryani@gmail.com>
2019-09-25 17:03:46 +0200

    hero CLI image == GIF instead of video

--- src/components/pages/cli/Hero/Image.js
+++ src/components/pages/cli/Hero/Image.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Box } from 'serverless-design-system'
 import '../video.css'
+import cliGif from 'src/assets/images/pages/cli/CLI-hero.gif'
+import styles from './HeroCLI.module.css'
 
 const HeroVideo = props => (
   <Box mt={[0, 0, 0, 0, '-20px', 23]}>
-    <video autoPlay loop muted className='cli-video'>
-      <source
-        src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/cli/CLI-hero.mp4'
-        type='video/mp4'
-      />
-    </video>
+    <img src={cliGif} className={`${styles.heroVideo}`}/>
   </Box>
 )
 
