73345a5067692e350ecf12e468b44b9c60227156
Nick Gottlieb <ngottlieb1@gmail.com>
2019-10-16 16:49:27 -0700

    pricing update

--- src/components/pages/company/jobs/OpenPositions/data.js
+++ src/components/pages/company/jobs/OpenPositions/data.js
@@ -8,24 +8,17 @@ export default [
     },
     {
         "heading": "Growth",
-        "title": "Director of Growth Marketing",
-        "location": "San Francisco/Remote",
-        "status": "Full-time",
-        "link": "https://jobs.lever.co/serverless/ec1a3ae3-3b51-4d1c-9709-696b6fab7970"
-    },
-    {
-        "heading": "Growth",
         "title": "Enterprise Account Executive",
         "location": "San Francisco/Remote",
         "status": "Full-time",
         "link": "https://jobs.lever.co/serverless/963e85b2-79f4-4b30-affc-37521724f33d"
     },
     {
-        "heading": "Growth",
-        "title": "Growth Marketing Manager",
+        "heading": "Engineering",
+        "title": "Senior Full Stack Engineer",
         "location": "San Francisco/Remote",
-        "status": "Contract",
-        "link": "https://jobs.lever.co/serverless/59b12b01-6052-407d-9bf8-cc019a65eafc"
+        "status": "Full-time",
+        "link": "https://jobs.lever.co/serverless/5a4fe96c-4251-4707-8762-b49f02478f8e"
     },
     {
         "heading": "Engineering",
