69ace539965073124a1cf4122bdcb91c48cf3717
Maciej Skierkowski <maciej@skierkowski.com>
2019-10-29 16:05:01 -0700

    Update hero style to match new layouts

--- src/components/pages/cicd/Hero/index.js
+++ src/components/pages/cicd/Hero/index.js
@@ -1,19 +1,48 @@
 import React from 'react'
-import { Flex } from 'serverless-design-system'
-import Content from './Content'
-import Image from './Image'
 import { HeroWrapper } from 'src/fragments'
+import { ExternalLink, InternalLink } from 'src/fragments'
+import { dashboard, pricing } from 'src/constants/urls.js'
+import { Button } from 'src/components'
+import HeroImage from 'src/assets/images/pages/cicd/cicd-hero.png'
+import FrameworkLogo from 'src/assets/images/logo-serverless-framework-center-horizontal-dark.png'
+import styles from '../CICD.module.css'
 
 const CiCdHero = props => (
-  <HeroWrapper customPb={[62, 62, 62, 62, 92, 184]}>
-    <Flex
-      justifyContent='space-between'
-      flexDirection={['column', 'column', 'column', 'column', 'row']}
-      alignItems={'center'}
-    >
-      <Content />
-      <Image />
-    </Flex>
+  <HeroWrapper
+    customPb={[62, 62, 62, 62, 92, 139]}>
+    <div className={styles.productHeroContainer}>
+      <div className={styles.productHeroColumnLeft}>
+        <img
+          src={FrameworkLogo}
+          alt={'Serverless Framework'}
+          className={styles.frameworkLogo} />
+        <div className={styles.productName}>
+          CI/CD
+        </div>
+        <div className={styles.productTagline}>
+          Test and deploy your serverless apps with ease!
+        </div>
+        <div className={styles.productCTA}>
+          <ExternalLink to={dashboard}>
+            <Button type='primary' text='Sign up for free'/>
+          </ExternalLink>
+        </div>
+        <div className={styles.productEdition}>
+          Available now in
+          <InternalLink
+            className={styles.editionLink}
+            to={pricing}>
+          Serverless Framework Pro.
+          </InternalLink>
+        </div>
+      </div>
+      <div className={styles.productHeroColumnRight}>
+        <img
+          src={HeroImage}
+          alt={'Serverless Framework CI/CD'}
+          className={styles.productHeroImage} />
+      </div>
+    </div>
   </HeroWrapper>
 )
 
