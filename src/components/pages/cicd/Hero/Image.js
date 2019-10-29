63660c56a0a1f4137bdd3a12ce1437820e2362be
Maciej Skierkowski <maciej@skierkowski.com>
2019-10-29 15:00:00 -0700

    Adding hero image

--- src/components/pages/cicd/Hero/Image.js
+++ src/components/pages/cicd/Hero/Image.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Box } from 'serverless-design-system'
+import cicdHeroImage from 'src/assets/images/pages/cicd/cicd-hero.png'
 import { ImageWithShadow as Image } from 'src/fragments'
 
 const HeroImage = props => (
   <Box mb={[0, 0, 0, 0, -56, -166]}>
     <Image
-      src={'http://www.fillmurray.com/1184/926'}
-      width={['100%', '100%', 488, 488, 488, 592]}
-      height={['78%', '78%', 382, 382, 382, 463]}
+      src={cicdHeroImage}
+      width={['100%', '100%', 488, 488, 488, 556]}
+      height={['61%', '61%', 299, 299, 299, 341]}
     />
   </Box>
 )
