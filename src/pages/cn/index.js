c9e3b8e88600dbc9f3d3ddda0e1de506bbe8d024
yugasun <yuga_sun@163.com>
2020-01-16 12:35:45 +0800

    fix: update cn index description

--- src/pages/cn/index.js
+++ src/pages/cn/index.js
@@ -10,7 +10,7 @@ const Homepage = ({ location }) => (
   <DefaultLayout pathName={location.pathname}>
   <Helmet
       title='Serverless 官方网站 - 立即使用Serverless，只需三步 !'
-      description='Github上最流行的Serverless开发平台，超1500万次下载，开源、免费、高效。全球11+云厂商共同支持，50余组件提供场景化的Serverless应用开发和运维体验，无缝支持Node.js Vue、React、Express、Koa、Egg、Ptyhon Flask、PHP Laravel等多语言开发框架。提供完善的Serverless开发教程，零基础入门。'
+      description='Github上最流行的Serverless开发平台，超1500万次下载，开源、免费、高效。轻松完成业务上云，50余组件提供场景化的Serverless应用开发和运维体验，无缝支持Node.js Vue、React、Express、Koa、Egg、Ptyhon Flask、PHP Laravel等多语言开发框架。提供完善的Serverless开发教程，零基础入门。'
       location={location}
       noIndex={true}
     />
